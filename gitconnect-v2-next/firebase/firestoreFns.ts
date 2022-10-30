e9b6be6c79d4790a439d0ff77213237e67303d02
Dannydoesdev <daniel.t.mcgee@gmail.com>
2022-10-30 22:38:41 +1100

    cleaned up unused code

--- gitconnect-v2-next/firebase/firestoreFns.ts
+++ gitconnect-v2-next/firebase/firestoreFns.ts
@@ -9,25 +9,6 @@ import {
   getDocs
 } from 'firebase/firestore'
 
-// const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE,
-//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-// }
-
-// let firebaseApp
-
-// // init app
-// if (!getApps().length) {
-//   firebaseApp = initializeApp(firebaseConfig)
-//   console.log('firebase initialised')
-//   // const analytics = getAnalytics(firebaseApp);
-// }
-
 // init firestore
 // const db = getFirestore(app);
 const fbdb = db
