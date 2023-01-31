cba45ba95a92c67a4f5f01f144394a0eaf68a5a1
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-01-31 23:32:16 +1100

    Added avatars and links to profile page on landing cards

--- gitconnect-v2-next/components/HomePageProjects/HomePageProjectGrid.tsx
+++ gitconnect-v2-next/components/HomePageProjects/HomePageProjectGrid.tsx
@@ -35,8 +35,16 @@ const HomePageProjectGrid = () => {
       {projects ?
         projects.map((project: any) => {
           return (
-            < div key={project.id} >
-              <ImageCard image={`../../../img/${project.id}.jpg` ? `../../../img/${project.id}.jpg` : (makeAnImg(600, 350))} title={project.name} author={project.owner.login} views={1} comments={2} link={`/profiles/projects/${project.id}`} />
+            <div key={project.id} >
+              <ImageCard
+                image={`../../../img/${project.id}.jpg` ? `../../../img/${project.id}.jpg` : (makeAnImg(600, 350))}
+                title={project.name}
+                author={project.owner.login}
+                views={1}
+                comments={2}
+                avatar={project.owner.avatar_url}
+                profileUrl={`/profiles/${project.userId}`}
+                link={`/profiles/projects/${project.id}`} />
             </div>
           )
         }) :
