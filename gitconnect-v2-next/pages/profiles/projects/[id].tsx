0ea2e61ce87d7aff6a67d039baca618cd114db10
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-02-11 00:13:12 +1100

    Added authentication check & update edit page to disallow editing someone elses project

--- gitconnect-v2-next/pages/profiles/projects/[id].tsx
+++ gitconnect-v2-next/pages/profiles/projects/[id].tsx
@@ -47,7 +47,7 @@ export default function Project() {
   }, [])
 
   if (projects) {
-  
+    
     return (
         <>
         <ProjectPageDynamicHero props={projects} />
--- gitconnect-v2-next/pages/profiles/projects/edit/[id].tsx
+++ gitconnect-v2-next/pages/profiles/projects/edit/[id].tsx
@@ -3,7 +3,7 @@
 // Copying in view-only version for boilerplate
 
 import axios from 'axios'
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useContext } from 'react'
 import useSWR from 'swr'
 import { getAllProjectIds, getProjectData } from '../../../../lib/projects'
 import Link from 'next/link'
@@ -14,11 +14,12 @@ import ProjectPageDynamicContent from '../../../../components/ProjectPageDynamic
 import { ProjectPageDynamicHero } from '../../../../components/ProjectPageDynamicHero/ProjectPageDynamicHero'
 
 import RichTextEditor from '../../../../components/RichTextEditor/RichTextEditor'
+import { AuthContext } from '../../../../context/AuthContext'
 
 // NOTE URL like http://localhost:3000/profiles/projects/edit/572895196
 
 export default function EditProject() {
-
+  const { userData } = useContext(AuthContext)
   const router = useRouter()
   // console.log(router.query)
   const { id } = router.query
@@ -41,28 +42,51 @@ export default function EditProject() {
   }, [])
 
   if (project) {
+
     const projectData = project[0]
+
+    if (projectData.userId === userData.userId) {
+
+      console.log(projectData.userId)
+      console.log(userData.userId)
+      return (
+        <>
+          <ProjectPageDynamicHero props={project} />
+          <Space h={40} />
+          <Title
+            order={1}
+            align="center"
+            mt="sm">
+            {projectData.name}
+          </Title>
+          <RichTextEditor />
+          {/* <ProjectPageDynamicContent props = {projects} /> */}
+        </>
+      )
+    } else {
+
+      // In case of signed in user not being project owner
+
+      return (
+        <>
+          <Group w='100%' mt={200}>
+            <Title
+              order={1}
+              mx='auto'
+              mt="sm">
+              Sorry, only the project owner is allowed to edit this page
+            </Title>
+          </Group>
+        </>
+      )
+    }
+  } else {
+    // In case of data still loading
 
     return (
       <>
-        <ProjectPageDynamicHero props={project} />
-        <Space h={40} />
-        <Title
-          order={1}
-          align="center"
-          mt="sm">
-          {projectData.name}
-        </Title>
-        <RichTextEditor />
-        {/* <ProjectPageDynamicContent props = {projects} /> */}
+        <h2> loading </h2>
       </>
     )
-  } else {
-
   }
-  return (
-    <>
-      <h2> loading </h2>
-    </>
-  )
 }
