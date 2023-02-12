f19516992cefcb474c544ba7fe0bd075913f170b
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-02-12 14:07:31 +1100

    Migrated updates to main Rich Text Editor component

--- components/RichTextEditor/RichTextEditor.tsx
+++ components/RichTextEditor/RichTextEditor.tsx
@@ -21,104 +21,47 @@ const templateContent =
   '<h2 style="text-align: center;">Welcome to GitConnect; rich text editor</h2><p style="text-align: center;">You can edit this box and use the toolbar above to style - <em>currently, your changes will not save on refresh</em></p><hr><p><code>RichTextEditor</code> component focuses on usability and is designed to be as simple as possible to bring a familiar editing experience to regular users. <code>RichTextEditor</code> is based on <a href="https://tiptap.dev/" rel="noopener noreferrer" target="_blank">Tiptap.dev</a> and supports all of its features:</p><ul><li>General text formatting: <strong>bold</strong>, <em>italic</em>, <u>underline</u>, <s>strike-through</s> </li><li>Headings (h1-h6)</li><li>Sub and super scripts (<sup>&lt;sup /&gt;</sup> and <sub>&lt;sub /&gt;</sub> tags)</li><li>Ordered and bullet lists</li><li>Text align&nbsp;</li><li>And all <a href="https://tiptap.dev/extensions" target="_blank" rel="noopener noreferrer">other extensions</a></li></ul>';
 
 
-// export async function getStaticProps({ repoId }: TipTapProps) {
-//   console.log('hi')
-//   const { userData } = useContext(AuthContext)
-//   // const res = await fetch(`https://www.swapi.tech/api/films/`)
-//   // const data = await res.json()
-
-//   let initialFirebaseData;
-
-//   const docRef = doc(db, `users/${userData.userId}/repos/${repoId}/projectData/mainContent`)
-//   console.log('testStaticProps')
-//   console.log(userData.userId)
-//   console.log(repoId)
-//   const docSnap = await getDoc(docRef);
-//   if (docSnap.exists()) {
-
-//     const mainContent = docSnap.data()
-//     const htmlOutput = mainContent.htmlOutput
-//     // console.log("Current document data:", docSnap.data());
-//     console.log("Current html data:", mainContent.htmlOutput);
-//     console.log(htmlOutput.length)
-
-//     if (htmlOutput.length > 0) {
-//       console.log('updating content')
-//       initialFirebaseData = htmlOutput;
-//       // setFirebaseData(htmlOutput)
-//       // setinitialContent(htmlOutput)
-//       // content = firebaseData;
-//       // console.log(content)
-//     } else { initialFirebaseData = templateContent; }
-//           // setFirebaseData()
-    
-//     // const newDocSnap = await getDoc(docRef);
-//     // console.log("New document data:", newDocSnap.data());
-//   } else {
-
-//     initialFirebaseData = templateContent;
-//     // setinitialContent(templateContent)
-
-//     // doc.data() will be undefined in this case
-//     console.log("No such document!");
-
-//   }
-//   // console.log(initialContent)
-//   return {
-//     props: {
-//       initialData: initialFirebaseData
-//       // films: data.result
-//     },
-//     revalidate: 100,
-//   };
-// }
-
-
 function TipTapEditor({ repoId, initialFirebaseData }: TipTapProps) {
   const { userData } = useContext(AuthContext)
-  
+
   const [editorContent, setEditorContent] = useState("");
   const [editable, setEditable] = useState(false)
   const [editorEnabled, setEditorEnabled] = useState(false);
   const [firebaseData, setFirebaseData] = useState('');
-  const [initialContent, setinitialContent] = useState('')
-  const content = initialContent;
+  const [initialContent, setinitialContent] = useState(templateContent)
+  const [content, setContent] = useState(templateContent);
 
-  console.log('initial data:' + initialFirebaseData)
 
-  async function getFirebaseData() {
-    console.log(initialContent)
-    const docRef = doc(db, `users/${userId}/repos/${repoId}/projectData/mainContent`)
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
+  // Load any existing data from Firestore & put in state
 
-      const mainContent = docSnap.data()
-      const htmlOutput = mainContent.htmlOutput
-      // console.log("Current document data:", docSnap.data());
-      console.log("Current html data:", mainContent.htmlOutput);
-      console.log(htmlOutput.length)
+  useEffect(() => {
 
-      if (htmlOutput.length > 0) {
-        console.log('updating content')
-        setFirebaseData(htmlOutput)
-        setinitialContent(htmlOutput)
-        // content = firebaseData;
-        console.log(content)
+    const getFirebaseData = async () => {
+
+      const docRef = doc(db, `users/${userId}/repos/${repoId}/projectData/mainContent`)
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        const mainContent = docSnap.data()
+        const htmlOutput = mainContent.htmlOutput
+
+        if (htmlOutput.length > 0) {
+          setinitialContent(htmlOutput);
+
+        }
       }
-            // setFirebaseData()
-      
-      // const newDocSnap = await getDoc(docRef);
-      // console.log("New document data:", newDocSnap.data());
-    } else {
 
-      setinitialContent(templateContent)
+    };
+    getFirebaseData();
+  }, []);
 
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
 
-    }
-    console.log(initialContent)
-    }
+  // Set the existing data as the editor content if found
+
+  useEffect(() => {
+    editor?.commands.setContent(initialContent);
+  }, [initialContent]);
+
 
   const editor = useEditor({
     editable,
@@ -131,16 +74,21 @@ function TipTapEditor({ repoId, initialFirebaseData }: TipTapProps) {
       Highlight,
       TextAlign.configure({ types: ['heading', 'paragraph'] }),
     ],
-    // content,
     content,
     onUpdate({ editor }) {
+
+      // Update state every time the editor content changes
       setEditorContent(editor.getHTML());
     },
   });
 
 
+  // One button currently for 'edit' and 'save'
+  // When clicking 'edit' this button will change the editor to 'editable'
+  // When updates are finished - clicking 'save' will send the html content to Firestore
+  // Checks whether the user was actually editing before sending
+
   function handleDoneAdding() {
-    console.log(editable);
     if (!editor) {
       return undefined
     }
@@ -150,88 +98,32 @@ function TipTapEditor({ repoId, initialFirebaseData }: TipTapProps) {
     }
 
     setEditable(!editable)
-
-    console.log(editable);
-
   }
-
-
-  useEffect(() => {
-
-    
-    getFirebaseData()
-
-    
-    
-    
-  }, [])
-
-
 
   const userId = userData.userId
   const userName = userData.userName
 
-  console.log(repoId)
-  console.log(userId)
+
+
+  // Updates whatever was in the 'htmlOutput' in Firestore with what is currently in the editor upon saving
+  // Note - to check if this creates a document even when the path doesn't exist yet
+
   async function sendContentToFirebase() {
 
-    // Note the format to find: 
-   // /users/bO4o8u9IskNbFk2wXZmjtJhAYkR2/repos/572895196/projectData
-    // WHERE DocumentID == mainContent
-
-        // map through selected repos
-    const projectRef = collection(db, `users/${userId}/repos/${repoId}/projectData`)
-    const q = query(projectRef, where('DocumentID', '==', 'mainContent'))
-
-          const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((doc) => {
-        // doc.data() is never undefined for query doc snapshots
-        console.log(doc.id, " => ", doc.data());
-      });
-
-
     const docRef = doc(db, `users/${userId}/repos/${repoId}/projectData/mainContent`)
-  
     const docSnap = await getDoc(docRef);
 
     if (docSnap.exists()) {
-      console.log("Current document data:", docSnap.data());
+      // console.log("Current document data:", docSnap.data());
+      // console.log('adding the following content to the document:')
+      // console.log(editorContent)
 
-      console.log('adding the following content to the document:')
-      console.log(editorContent)
       await setDoc(docRef, { htmlOutput: editorContent }, { merge: true });
-      const newDocSnap = await getDoc(docRef);
-      console.log("New document data:", newDocSnap.data());
+      // const newDocSnap = await getDoc(docRef);
+      // console.log("New document data:", newDocSnap.data());
     } else {
-      // doc.data() will be undefined in this case
       console.log("No such document!");
     }
-
-      // .id('MainContent')
-        // const q = query(projectRef, where('DocumentID', '==', 'mainContent'))
-
-          // the document of the user in the users collection
-          // const rootRef = collection(db, 'users')
-          // const repoName = repoData.name;
-          // const repoId = repoData.id
-          // const docRef = doc(db, 'users', userId);
-          // const q = query(collection(db, 'users'), where('userName', '==', userName));
-          // const querySnapshot = await getDocs(q);
-          // const queryData = querySnapshot.docs.map((detail: any) => {
-          // });
-    
-          // console.log(queryData);
-    
-    
-          // queryData.map(async (v) => {
-          //   // console.log(v)
-          //   await setDoc(doc(db, `users/${userId}/repos/${repoId}`), { ...repoData, userId: userId  }, { merge: true })
-          //     .then(() => {
-          //       console.log(`Repo ${repoName} added to firestore under user ${userName} with ID: , ${repoId}`);
-          //     })
-          //     .catch((e) => { console.log(e); })
-          // })
-  
 
   }
 
@@ -341,45 +233,3 @@ function TipTapEditor({ repoId, initialFirebaseData }: TipTapProps) {
 }
 
 export default TipTapEditor
-
-// Note the format to find: 
-// /users/bO4o8u9IskNbFk2wXZmjtJhAYkR2/repos/572895196/projectData
-// WHERE DocumentID == mainContent
-
-//   const newRepo = (repoName: string, repo: any, isChecked: boolean) => {
-    // add the selected repo to the selected repo state array
-  //   setAddRepoData([...addRepoData, repo])
-  //   // console.log(addRepoData)
-  // }
-
-  // const userId = userData.userId
-  // const userName = userData.userName
-  // // handle when user hits the 'done' button
-  // const handleDoneAdding = () => {
-
-  //   // map through selected repos
-  //   addRepoData.map(async (repoData: any) => {
-  //     // the document of the user in the users collection
-  //     // const rootRef = collection(db, 'users')
-  //     const repoName = repoData.name;
-  //     const repoId = repoData.id
-  //     const docRef = doc(db, 'users', userId);
-  //     const q = query(collection(db, 'users'), where('userName', '==', userName));
-  //     const querySnapshot = await getDocs(q);
-  //     const queryData = querySnapshot.docs.map((detail: any) => {
-  //     });
-
-  //     // console.log(queryData);
-  //     queryData.map(async (v) => {
-  //       // console.log(v)
-  //       //Removing the createdAt timestamp - was breaking the code
-  //       //createdAt: serverTimestamp()
-  //       await setDoc(doc(db, `users/${userId}/repos/${repoId}`), { ...repoData, userId: userId  }, { merge: true })
-  //         .then(() => {
-  //           console.log(`Repo ${repoName} added to firestore under user ${userName} with ID: , ${repoId}`);
-  //         })
-  //         .catch((e) => { console.log(e); })
-  //     })
-  //   })
-  //   Router.push('/')
-  // }
