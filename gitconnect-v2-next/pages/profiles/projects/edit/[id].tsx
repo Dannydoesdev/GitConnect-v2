7a2f26e97933b032e1827fe9ad2271701a9acc4f
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-03-05 13:44:34 +1100

    installed octokit - POC version of getReadme implemented

--- gitconnect-v2-next/pages/profiles/projects/edit/[id].tsx
+++ gitconnect-v2-next/pages/profiles/projects/edit/[id].tsx
@@ -19,8 +19,11 @@ export default function EditProject() {
   const { id } = router.query
 
   const [project, setProject] = useState<any>(null)
+  const [readme, setReadme] = useState('')
 
   useEffect(() => {
+
+    // TODO - implement Vercel SWR on front end
 
     const URL = `/api/profiles/projects/${id}`;
     axios.get(URL)
@@ -28,8 +31,31 @@ export default function EditProject() {
         // console.log(response.data)
         setProject(response.data)
       })
-
+    
+    
+    
   }, [])
+
+  useEffect(() => {
+    
+    if (id && project) {
+      // const stringId: string = id.toString()
+      
+
+      const readmeUrl = `/api/profiles/projects/edit/readme`;
+      axios.get(readmeUrl, {
+        params: {
+          owner: userData.userName,
+          repo: project[0].name,
+        }
+      })
+        .then((response) => {
+          console.log(response.data)
+          setReadme(response.data)
+        })
+    }
+
+  }, [project])
 
   if (project) {
 
