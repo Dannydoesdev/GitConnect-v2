7885f53651d7810dd67a7c901eaccd170fb7f965
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-04-02 09:04:35 +1000

    Successfully pulling comprehensive public github profile info - to layout on profile page

---
+++ gitconnect-v2-next/pages/grid.tsx
@@ -0,0 +1,14 @@
+import { Space } from "@mantine/core";
+import { ProfilePageLayoutGrid } from "../components/ProfilePage/ProfilePageLayout/ProfilePageLayout";
+
+function ProfilePageGrid() {
+
+  return (
+    <>
+      <Space h={70} />
+      <ProfilePageLayoutGrid />
+    </>
+  )
+};
+
+export default ProfilePageGrid;
