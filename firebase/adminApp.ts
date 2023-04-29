39df707503645e686e7906c94b9ca121af4dbe34
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-04-29 23:18:51 +1000

    console log cleanup

--- firebase/adminApp.ts
+++ firebase/adminApp.ts
@@ -19,20 +19,3 @@ export const adminDb = getFirestore(adminApp);
 
 export const adminAuth = getAuth(adminApp);
 
-console.log(adminApp.name);
-
-
-// import { config } from 'dotenv';
-
-// config();
-
-// if (!process.env.FIREBASE_PROJECT_ID || !process.env.FIREBASE_ADMIN_CLIENT_EMAIL || !process.env.FIREBASE_ADMIN_PRIVATE_KEY || !process.env.FIREBASE_DATABASE_URL) {
-//   console.error('Error: Missing required Firebase environment variables');
-// } else {
-//   const serviceAccount = {
-//     projectId: process.env.FIREBASE_PROJECT_ID,
-//     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//     privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
-//   };
-
-// }
