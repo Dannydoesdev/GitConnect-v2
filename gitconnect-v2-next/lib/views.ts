791dbacca3160322ae1d4be1c4ab1f69b7d44e72
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-04-29 19:33:44 +1000

    working on updating viewcounts using firebase admin sdk

---
+++ gitconnect-v2-next/lib/views.ts
@@ -0,0 +1,11 @@
+import { doc, updateDoc, increment } from "firebase/firestore"; 
+import { db } from '../firebase/clientApp';
+
+
+export async function incrementViewCount(projectId: string) {
+  const projectRef = doc(db, 'repos', projectId);
+
+  await updateDoc(projectRef, {
+    views: increment(1)
+  });
+}
