d89651d665346f68a006d484a97b5fe1d5b3ee89
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-05-19 03:29:45 +1000

    Update edit page - buttons & layout

---
+++ components/ProjectEditPage/ProjectEditPageHero/ProjectEditPageHero.tsx
@@ -0,0 +1,72 @@
+import axios from 'axios'
+import { useState, useEffect } from 'react'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { createStyles, Overlay, Container, Title, Avatar, Switch, Card, Image, Text, SimpleGrid, Badge, Button, Group, Space, Center, Stack } from '@mantine/core';
+import useStyles from './ProjectEditPageHero.styles';
+
+export function ProjectEditPageHero(props: any) {
+  const { classes } = useStyles();
+  // console.log('Dynamic project hero')
+  // console.log(props)
+  function makeAnImg(width: number, height: number) {
+    let randNum = Math.floor(Math.random() * 1000)
+    return `https://picsum.photos/${width}/${height}?random=${randNum}`
+  }
+
+
+  const project = props.props[0];
+
+  return (
+    // <div className={classes.hero}>
+    // {/* </div> */}
+    <Group className={classes.hero} sx={{ backgroundImage: project.coverImage ? `url(${project.coverImage})` : `url(${makeAnImg(3200, 1200)})` }}>
+      <Overlay
+        gradient="linear-gradient(180deg, rgba(0, 0, 0, 0.25) 0%, rgba(0, 0, 0, .65) 40%)"
+        opacity={1}
+        zIndex={0}
+      />
+
+      <Container className={classes.container}>
+        <Title className={classes.title}>Edit {project.name}</Title>
+        <Text className={classes.description} size="xl" mt="xl">
+          {/* {project.name} */}
+        </Text>
+        <Group className={classes.group} grow>
+          {project.live_url &&
+            <Link href={project.live_url ? project.live_url : ''} passHref legacyBehavior>
+              <Button
+                component="a"
+                target='_blank'
+                // size='xl'
+                // radius='xl'
+                className={classes.liveAndGithub}
+                sx={(theme) => ({
+                  // subscribe to color scheme changes
+                  backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.blue[6],
+                })}
+              >Live site</Button>
+            </Link>
+          }
+          <Link href={project.html_url} passHref legacyBehavior>
+            <Button
+              component="a"
+              target='_blank'
+              // size='xl'
+              // radius='xl'
+              className={project.live_url ? classes.liveAndGithub : classes.githubOnly}
+              sx={(theme) => ({
+                // subscribe to color scheme changes
+                backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.blue[6],
+              })}
+            >On GitHub</Button>
+          </Link>
+
+        </Group>
+        {/* <Button variant="gradient" size="xl" radius="xl" className={classes.control}>
+        Check it out!
+      </Button> */}
+      </Container>
+    </Group>
+  );
+}
