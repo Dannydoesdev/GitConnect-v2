6c0b91df3dbf074b4c8b1c2bf3a0fbded466f068
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-05-19 15:34:56 +1000

    Increases number of repos fetched by API and improved layout visibility

--- lib/github.ts
+++ lib/github.ts
@@ -11,6 +11,26 @@ export async function getGithubProfileData(username: string) {
     return null;
   }
 }
+
+export async function getGithubReposWithUsername(username: string) {
+  const octokit = new Octokit();
+
+  try {
+    const response = await octokit.repos.listForUser({
+      username: username,
+      sort: 'updated',
+      per_page: 100,
+    })
+    
+      return response.data;
+
+  } catch (error) {
+    console.error(`Failed to get GitHub data for ${username}`, error);
+    return null;
+  }
+};
+
+
   // return await octokit.users
   //   .getByUsername({
   //     username,
