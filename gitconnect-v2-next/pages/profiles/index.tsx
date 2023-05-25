52c562d1499b8552c8bbc571a96cc8e781a13a07
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-05-26 01:31:05 +1000

    removed index pages of profiles and projects

--- gitconnect-v2-next/pages/profiles/index.tsx
+++ gitconnect-v2-next/pages/profiles/index.tsx
@@ -1,50 +1,52 @@
-import styles from '../../styles/Ninjas.module.css'
-import Link from 'next/link'
-import { getAllProfileIds, getProfileData } from '../../lib/profiles';
-import axios from 'axios'
-import useSWR from 'swr'
+export { }
 
-export async function getStaticProps() {
+// import styles from '../../styles/Ninjas.module.css'
+// import Link from 'next/link'
+// import { getAllProfileIds, getProfileData } from '../../lib/profiles';
+// import axios from 'axios'
+// import useSWR from 'swr'
 
-  const pages: any = await getAllProfileIds();
-  // const data = await pages
+// export async function getStaticProps() {
 
-  // console.log(data)
-  // .then(() => {})
-  // console.log('PAGES')
-  // console.log(pages)
-  // console.log({ ...pages })
-  // const pageData = await pages.forEach((page: any) => {
-  //   getProfileData(page)
-  // })
-  // console.log(pageData)
+//   const pages: any = await getAllProfileIds();
+//   // const data = await pages
 
-  return {
-    props: {
-      profiles: pages
-    },
-    revalidate: 10,
-  }
-}
+//   // console.log(data)
+//   // .then(() => {})
+//   // console.log('PAGES')
+//   // console.log(pages)
+//   // console.log({ ...pages })
+//   // const pageData = await pages.forEach((page: any) => {
+//   //   getProfileData(page)
+//   // })
+//   // console.log(pageData)
 
-
-const Profiles = ({ profiles }: any) => {
-  return (
-    <div>
-      <h1>All Profiles</h1>
-      {profiles.map((profile: any) => {
-        return <>
-          <h1>{profile.params.id}</h1>
-          <Link href={'/profiles/' + profile.params.id} key={profile.params.id}>
-
-            <h3>Go to Profile</h3>
-
-          </Link>
-        </>
-      })}
-    </div>
-  );
-}
+//   return {
+//     props: {
+//       profiles: pages
+//     },
+//     revalidate: 10,
+//   }
+// }
 
 
-export default Profiles;
+// const Profiles = ({ profiles }: any) => {
+//   return (
+//     <div>
+//       <h1>All Profiles</h1>
+//       {profiles.map((profile: any) => {
+//         return <>
+//           <h1>{profile.params.id}</h1>
+//           <Link href={'/profiles/' + profile.params.id} key={profile.params.id}>
+
+//             <h3>Go to Profile</h3>
+
+//           </Link>
+//         </>
+//       })}
+//     </div>
+//   );
+// }
+
+
+// export default Profiles;
