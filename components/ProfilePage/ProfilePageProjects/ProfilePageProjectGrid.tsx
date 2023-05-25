58e89f32d2d2811672b7bb5a3e6886570197849a
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-05-26 03:15:49 +1000

    Cleanup files

--- components/ProfilePage/ProfilePageProjects/ProfilePageProjectGrid.tsx
+++ components/ProfilePage/ProfilePageProjects/ProfilePageProjectGrid.tsx
@@ -1,7 +1,6 @@
-import React, { useContext, useState } from "react"
-import { ProfilePageProjectCard } from './ProfilePageProjectCard'
-import { SimpleGrid } from '@mantine/core'
-
+import React, { useContext, useState } from 'react';
+import { ProfilePageProjectCard } from './ProfilePageProjectCard';
+import { SimpleGrid } from '@mantine/core';
 
 const ProfilePageProjectGrid = ({ projects }: any) => {
   // const { classes, theme } = useStyles();
@@ -21,26 +20,26 @@ const ProfilePageProjectGrid = ({ projects }: any) => {
   //     })
 
   // }, [])
- 
 
   return (
-
-    <SimpleGrid cols={4} spacing="md" breakpoints={[
-      { maxWidth: 2000, cols: 3, spacing: 'sm' },
-      { maxWidth: 1200, cols: 2, spacing: 'sm' },
-      { maxWidth: 600, cols: 1, spacing: 'sm' },
-    ]}>
-
-      {projects ?
+    <SimpleGrid
+      cols={4}
+      spacing='md'
+      breakpoints={[
+        { maxWidth: 2000, cols: 3, spacing: 'sm' },
+        { maxWidth: 1200, cols: 2, spacing: 'sm' },
+        { maxWidth: 600, cols: 1, spacing: 'sm' },
+      ]}
+    >
+      {projects ? (
         projects.map((project: any, index: any) => {
           // console.log(project.docData.coverImage)
           // const { imageUrl }: any = useViewportForImageSize(project.docData.coverImage, userId, project.docData.id);
 
           // console.log(imageUrl)
-          
+
           return (
-            <div key={project.docData.id} >
-              
+            <div key={project.docData.id}>
               <ProfilePageProjectCard
                 hidden={project.docData.hidden}
                 image={project.docData.coverImage}
@@ -51,16 +50,16 @@ const ProfilePageProjectGrid = ({ projects }: any) => {
                 views={1}
                 comments={2}
                 profileUrl={`/profiles/${project.docData.userId}`}
-                link={`/profiles/projects/${project.docData.id}`} />
+                link={`/profiles/projects/${project.docData.id}`}
+              />
             </div>
-          )
-        }) :
+          );
+        })
+      ) : (
         <h2>Loading projects</h2>
-      }
+      )}
+    </SimpleGrid>
+  );
+};
 
-    </SimpleGrid >
-
-  )
-}
-
-export default ProfilePageProjectGrid
+export default ProfilePageProjectGrid;
