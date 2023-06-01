64c761efc91a42f603b3155272ccb1dd0dd2f10f
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-06-01 16:31:12 +1000

    New single project creation flow

--- pages/addproject.tsx
+++ pages/addproject.tsx
@@ -3,10 +3,8 @@ import Link from 'next/link';
 import React, { useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { AuthContext } from '../context/AuthContext';
-import axios from 'axios';
 import {
   Avatar,
-  Switch,
   Card,
   Text,
   SimpleGrid,
@@ -14,27 +12,17 @@ import {
   Button,
   Group,
   Space,
-  Center,
   Stack,
-  useMantineTheme,
-  Box,
   Blockquote,
 } from '@mantine/core';
 import { db } from '../firebase/clientApp';
 import {
   collection,
   setDoc,
-  addDoc,
-  where,
   query,
-  getDoc,
   doc,
-  serverTimestamp,
   getDocs,
-  Timestamp
-  // FieldValue,
 } from 'firebase/firestore';
-import { IconCheck, IconLineDashed, IconX } from '@tabler/icons-react';
 import { RepoDataFull } from '../types/repos';
 import { getGithubReposWithUsername } from '../lib/github';
 import { InfoCircle } from 'tabler-icons-react';
@@ -43,14 +31,12 @@ interface ShowRepoProps {
   repo: RepoDataFull;
   existingRepos: string[];
   addRepo: (repo: RepoDataFull) => void;
-  removeRepo: (repo: RepoDataFull) => void;
 }
 
 const ShowRepo: React.FC<ShowRepoProps> = ({
   repo,
   existingRepos,
   addRepo,
-  removeRepo,
 }) => {
   const {
     name: repoName,
@@ -59,23 +45,11 @@ const ShowRepo: React.FC<ShowRepoProps> = ({
     description: repoDesc,
     license: repoLicense,
   } = repo;
-  const theme = useMantineTheme();
-  const [isChecked, setIsChecked] = useState(false);
 
   const repoAlreadyAdded = existingRepos.includes(repo.id.toString());
 
-  const handleCheck = (checked: boolean) => {
-    setIsChecked(checked);
-    if (checked) {
-      addRepo(repo);
-    } else {
-      removeRepo(repo);
-    }
-  };
-
   return (
     <div>
-      {/* <Box h={200}> */}
       <Card shadow='sm' p='lg' radius='md' withBorder>
         <Card.Section></Card.Section>
 
@@ -96,14 +70,11 @@ const ShowRepo: React.FC<ShowRepoProps> = ({
             </Badge>
           )}
         </Group>
-        {/* <Box w={200}>  <Card.Section> */}
         <Text truncate size='sm' color='dimmed'>
           {repoDesc
             ? repoDesc
             : 'No description found - you can add a custom description with GitConnect once you add this repo'}
         </Text>
-        {/* </Card.Section> */}
-        {/* </Box> */}
         <Space h='xs' />
         <Text size='xs' color='dimmed'>
           {repoLicense
@@ -128,34 +99,24 @@ const ShowRepo: React.FC<ShowRepoProps> = ({
           </Group>
         ) : (
           <Group position='center'>
-            <Switch
-              onChange={(event) => handleCheck(event.currentTarget.checked)}
-              labelPosition='left'
-              label='Add to portfolio'
-              mt='lg'
-              size='md'
-              radius='lg'
-              color='green'
-              thumbIcon={
-                isChecked ? (
-                  <IconCheck
-                    size={12}
-                    color={theme.colors.teal[theme.fn.primaryShade()]}
-                    stroke={3}
-                  />
-                ) : (
-                  <IconLineDashed
-                    size={12}
-                    color={theme.colors.yellow[theme.fn.primaryShade()]}
-                    stroke={3}
-                  />
-                )
-              }
-            />
+            <Link href='#' passHref legacyBehavior>
+              <Button
+                component='a'
+                mt='xl'
+                size='xs'
+                // size='l'
+                radius='lg'
+                color='teal'
+                onClick={() => {
+                  addRepo(repo);
+                }}
+              >
+                Add {repoName}
+              </Button>
+            </Link>
           </Group>
         )}
       </Card>
-      {/* </Box> */}
     </div>
   );
 };
@@ -164,70 +125,49 @@ const GetRepos = () => {
   const { userData } = useContext(AuthContext);
   const { userId, userName } = userData;
   const [repoData, setRepoData] = useState<RepoDataFull[] | null>([]);
-  const [addRepoData, setAddRepoData] = useState<RepoDataFull[]>([]);
   const [existingRepos, setExistingRepos] = useState<string[]>([]);
   const router = useRouter();
 
-  const addRepo = (repo: RepoDataFull) => {
-    setAddRepoData((currentRepos) => [...currentRepos, repo]);
-  };
+  const addRepo = async (repo: RepoDataFull) => {
+    try {
+      await setDoc(
+        doc(db, `users/${userId}/repos/${repo.id}`),
+        {
+          ...repo,
+          userId,
+          hidden: true,
+          gitconnect_created_at: new Date().toISOString(),
+          gitconnect_updated_at: new Date().toISOString(),
+          gitconnect_created_at_unix: Date.now(),
+          gitconnect_updated_at_unix: Date.now(),
+        },
+        { merge: true }
+      );
 
-  const removeRepo = (repo: RepoDataFull) => {
-    setAddRepoData((currentRepos) =>
-      currentRepos.filter((currentRepo) => currentRepo.id !== repo.id)
-    );
-  };
-
-  const handleDoneAdding = async () => {
-    // If no repos were added, redirect to profile page without added parameters for timeout
-    if (!addRepoData.length) {
-      router.push(`/profiles/${userId}`);
-    } else {
-      try {
-        const promises = addRepoData.map(async (repo) => {
-          await setDoc(
-            doc(db, `users/${userId}/repos/${repo.id}`),
-            // { ...repo, userId, hidden: true },
-            { 
-              ...repo, 
-              userId, 
-              hidden: true, 
-              gitconnect_created_at: new Date().toISOString(),
-              gitconnect_updated_at: new Date().toISOString(),
-              gitconnect_created_at_unix: Date.now(),
-              gitconnect_updated_at_unix: Date.now(),
-            },
-            { merge: true }
-          );
-        });
-        await Promise.all(promises);
-
-        router.push(
-          {
-            pathname: `/profiles/${userId}`,
-            query: {
-              newRepoParam: JSON.stringify(true),
-            },
+      router.push(
+        {
+          pathname: `/portfolio/new/${repo.id}`,
+          query: {
+            name: repo.name,
+            description: repo.description,
+            url: repo.html_url,
+            userId: userId,
           },
-          `/profiles/${userId}`
-        );
-      } catch (err) {
-        console.error(err);
-      }
+        },
+        `/portfolio/new/${repo.id}`
+      );
+    } catch (err) {
+      console.error(err);
     }
   };
 
   useEffect(() => {
+
     const fetchData = async () => {
       try {
-        // Fetch GitHub data
-        // const { data } = await axios.get(`https://api.github.com/users/${userName}/repos`);
-        // setRepoData(data);
-
         const returnedRepoData = await getGithubReposWithUsername(userName);
         setRepoData(returnedRepoData);
 
-        // Fetch Firestore data
         const q = query(collection(db, `users/${userId}/repos`));
         const querySnapshot = await getDocs(q);
 
@@ -236,12 +176,12 @@ const GetRepos = () => {
           existingRepoArr.push(doc.id);
         });
 
-        // If there are existing repo added, set existingRepos state
         setExistingRepos(existingRepoArr);
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
+
     if (userName && userId) {
       fetchData();
     }
@@ -265,39 +205,11 @@ const GetRepos = () => {
           color='indigo'
           icon={<InfoCircle size='1.5rem' />}
         >
-          Toggle repos you'd like to add to your profile then hit "Done Adding"
+          Choose which project you want to add to your portfolio <br /> You will
+          be able to customise the project details in the next step
         </Blockquote>
       </Stack>
       <Space h='xl' />
-      <Space h='xl' />
-      <Group position='center'>
-        <Link href='#' passHref legacyBehavior>
-          <Button
-            component='a'
-            size='lg'
-            radius='md'
-            onClick={() => {
-              handleDoneAdding();
-            }}
-            className='mx-auto'
-            sx={(theme) => ({
-              // subscribe to color scheme changes
-              backgroundColor:
-                theme.colorScheme === 'dark'
-                  ? theme.colors.green[9]
-                  : theme.colors.green[6],
-              '&:hover': {
-                backgroundColor:
-                  theme.colorScheme === 'dark'
-                    ? theme.colors.green[8]
-                    : theme.colors.green[5],
-              },
-            })}
-          >
-            Done Adding
-          </Button>
-        </Link>
-      </Group>
       <Space h='xl' />
       <Group mx='md'>
         <SimpleGrid
@@ -316,7 +228,6 @@ const GetRepos = () => {
                   key={repo.id}
                   existingRepos={existingRepos}
                   addRepo={addRepo}
-                  removeRepo={removeRepo}
                   repo={repo}
                 />
               );
