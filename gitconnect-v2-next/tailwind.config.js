8f85294efadb7f3c5b8a552ab3ebb61005459555
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-06-07 21:16:58 +1000

    Added all file updates - next to install missing packages

---
+++ gitconnect-v2-next/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  mode: 'jit',
+  content: ['./src/**/*.tsx'],
+  // purge: ['./src/**/*.tsx'],
+  theme: {
+    extend: {
+      colors: {
+        electric: '#db00ff',
+        ribbon: '#0047ff',
+      },
+    },
+  },
+  plugins: [],
+}
