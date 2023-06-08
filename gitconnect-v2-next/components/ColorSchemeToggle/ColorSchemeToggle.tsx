20a1f91cb29066eab85b422f6ef7ce26469ae79b
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-06-08 21:06:43 +1000

    updated color switch to allow tailwind darkmode

--- gitconnect-v2-next/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ gitconnect-v2-next/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,37 +1,62 @@
-import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core'
-import { useHotkeys } from '@mantine/hooks'
-import { IconMoonStars, IconSun } from '@tabler/icons-react'
-import { useTheme } from 'next-themes'
-import tw from 'twin.macro'
+import { ActionIcon, Group, useMantineColorScheme } from '@mantine/core';
+import { useHotkeys } from '@mantine/hooks';
+import { IconMoon, IconMoonStars, IconSun } from '@tabler/icons-react';
+import { useTheme } from 'next-themes';
+import { Sun, Moon } from 'tabler-icons-react';
+import tw from 'twin.macro';
 
 export function ColorSchemeToggle() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
-  const dark = colorScheme === 'dark'
-  const { theme, setTheme } = useTheme()
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const dark = colorScheme === 'dark';
+  const { theme, setTheme } = useTheme();
 
   function setThemes() {
     if (theme !== colorScheme) {
-      setTheme(colorScheme)
+      setTheme(colorScheme);
     }
-    toggleColorScheme()
-    dark ? setTheme('light') : setTheme('dark')
+    toggleColorScheme();
+    dark ? setTheme('light') : setTheme('dark');
   }
-  useHotkeys([['mod+J', () => setThemes()]])
+  useHotkeys([['mod+J', () => setThemes()]]);
 
   return (
-    <Group position="center" mt="xl">
+    <Group position="center">
       <ActionIcon
+         aria-label="light or dark mode toggle"
         onClick={() => setThemes()}
-        size="xl"
-        color={dark ? 'yellow' : 'blue'}
-        css={[dark ? tw`bg-gray-700/30` : tw`bg-gray-300/30`]}
+        size="sm"
+        sx={(theme) => ({
+          backgroundColor: theme.colorScheme === "dark" ? theme.colors.dark[6] : theme.colors.gray[0],
+          color: theme.colorScheme === "dark" ? theme.colors.yellow[4] : theme.colors.blue[6],
+        })}
+        // color={dark ? 'yellow' : 'blue'}
+        // css={[dark ? tw`bg-gray-700/30` : tw`bg-gray-300/30`]}
       >
         {dark ? (
-          <IconSun size={20} stroke={1.5} />
+          // <IconSun size={15} />
+          <Sun size={15} />
         ) : (
-          <IconMoonStars size={20} stroke={1.5} />
+          // <IconMoon size={15} />
+          <Moon size={15} />
         )}
       </ActionIcon>
     </Group>
-  )
+  );
+
+  // return (
+  //   <Group position="center" mt="xl">
+  //     <ActionIcon
+  //       onClick={() => setThemes()}
+  //       size="xl"
+  //       color={dark ? 'yellow' : 'blue'}
+  //       css={[dark ? tw`bg-gray-700/30` : tw`bg-gray-300/30`]}
+  //     >
+  //       {dark ? (
+  //         <IconSun size={20} stroke={1.5} />
+  //       ) : (
+  //         <IconMoonStars size={20} stroke={1.5} />
+  //       )}
+  //     </ActionIcon>
+  //   </Group>
+  // )
 }
