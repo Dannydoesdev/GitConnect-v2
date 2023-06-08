1e9ca83da426f581cb69f4d626ef7fe82b83d2a6
Dannydoesdev <daniel.t.mcgee@gmail.com>
2023-06-08 17:58:54 +1000

    Updates to allow image alignment to work properly, icons showing etc

--- gitconnect-v2-next/prettier.config.js
+++ gitconnect-v2-next/prettier.config.js
@@ -1,7 +1,8 @@
 /** @type {import('prettier').Config} */
 module.exports = {
+  // plugins: ['@trivago/prettier-plugin-sort-imports'],
   endOfLine: 'lf',
-  semi: false,
+  semi: true,
   singleQuote: true,
   tabWidth: 2,
   trailingComma: 'es5',
@@ -16,13 +17,12 @@ module.exports = {
     '^@/lib/(.*)$',
     '^@/components/(.*)$',
     '^@/styles/(.*)$',
+    '.css$',
+    '.scss$',
     '^[./]',
+    // '',
   ],
-  importOrderSeparation: false,
-  importOrderSortSpecifiers: true,
-  importOrderBuiltinModulesToTop: true,
-  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
-  importOrderMergeDuplicateImports: true,
-  importOrderCombineTypeAndValueImports: true,
+  importOrderParserPlugins: ['typescript', 'jsx', 'classProperties', 'decorators-legacy'],
   plugins: ['@ianvs/prettier-plugin-sort-imports'],
-}
+  // overrides: [{ files: ['*.ts'], options: { parser: 'babel-ts' } }],
+};
