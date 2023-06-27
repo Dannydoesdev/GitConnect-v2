0bfeb80db6e9d4d894e854f9ac348a1880510c2d
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-06-27 22:29:48 +1000

    Modal added on save with projects information, tagging etc

---
+++ components/Portfolio/EditProjectSettings.tsx
@@ -0,0 +1,499 @@
+import React, { useContext, useState } from 'react';
+import { useRouter } from 'next/router';
+import {
+  Modal,
+  Paper,
+  Textarea,
+  Space,
+  Container,
+  Group,
+  Button,
+  TextInput,
+  ScrollArea,
+  Checkbox,
+  Stack,
+  Grid,
+} from '@mantine/core';
+import '@mantine/core';
+import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+import axios from 'axios';
+import { doc, setDoc } from 'firebase/firestore';
+import useSWR from 'swr';
+import { AuthContext } from '../../context/AuthContext';
+
+interface ProjectSettingsSimpleProps {
+  repoId?: string;
+  repoName?: string;
+  opened?: boolean;
+  open?: () => void;
+  close?: () => void;
+}
+
+interface ProjectSettingsProps {
+  repoId?: string;
+  repoName?: string;
+  opened: boolean;
+  open: () => void;
+  close: () => void;
+  handlePublish?: () => void;
+  handleSaveAsDraft?: () => void;
+  projectTitle?: string;
+  techStack?: string[];
+  projectTags?: string[];
+  liveUrl?: string;
+  repoUrl?: string;
+  projectCategories?: string[];
+  visibleToPublic?: boolean;
+  openToCollaboration?: boolean;
+  coverImage?: string;
+  projectDescription?: string;
+}
+
+function ProjectSettingsModal({
+  repoName,
+  repoId,
+  opened,
+  open,
+  close,
+  handlePublish,
+  handleSaveAsDraft,
+  techStack,
+  projectTags,
+  liveUrl,
+  repoUrl,
+  projectCategories,
+  visibleToPublic,
+  openToCollaboration,
+  coverImage,
+  projectDescription,
+}: ProjectSettingsProps) {
+  // function ProjectSettingsModal(props: ProjectSettingsProps) {
+  // const {
+  //   repoName,
+  //   repoId,
+  //   opened,
+  //   open,
+  //   close,
+  //   techStack,
+  //   projectTags,
+  //   liveUrl,
+  //   repoUrl,
+  //   projectCategories,
+  //   visibleToPublic,
+  //   openToCollaboration,
+  //   coverImage,
+  //   projectDescription,
+  // } = props;
+
+  // const [opened, { open, close }] = useDisclosure(false);
+  const [categoryValue, setCategoryValue] = useState<string[]>([]);
+
+  const [visibleToPublicValue, setVisibleToPublicValue] = useState<boolean>(false);
+  const [openToCollaborationValue, setOpenToCollaborationValue] =
+    useState<boolean>(false);
+
+  const form = useForm({
+    initialValues: {
+      projectTitle: repoName || '',
+      techStack: techStack || [],
+      projectTags: projectTags || [],
+      liveUrl: liveUrl || '',
+      repoUrl: repoUrl || '',
+      projectCategories: projectCategories || [],
+      visibleToPublic: visibleToPublic || false,
+      openToCollaboration: openToCollaboration || false,
+      coverImage: coverImage || '',
+      projectDescription: projectDescription || '',
+    },
+    // `${repoName}`,
+    // techStack: `${techStack}`,
+    // projectTags: `${projectTags}`,
+    //   liveUrl: `${liveUrl}`,
+    //   repoUrl: `${repoUrl}`,
+    //   projectCategories: `${projectCategories}`,
+    //   visibleToPublic: `${visibleToPublic}`,
+    //   openToCollaboration: `${openToCollaboration}`,
+    //   coverImage: `${coverImage}`,
+    //   projectDescription: `${projectDescription}`,
+    // },
+  });
+
+  return (
+    <>
+      {/* <Modal.Root opened={opened} onClose={close}>
+        <Modal.Overlay />
+        <Modal.Content>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+            <Modal.CloseButton />
+          </Modal.Header>
+          <Modal.Body>Modal content</Modal.Body>
+        </Modal.Content>
+      </Modal.Root> */}
+      <Modal
+        size="xl"
+        // px='xl'
+        // mx='lg'
+        // my='xl'
+        opened={opened}
+        onClose={close}
+        title="Project Settings"
+        scrollAreaComponent={ScrollArea.Autosize}
+        centered
+      >
+        <Grid gutter="lg">
+          <Grid.Col span={4}>{/* <Button>Cover Image</Button> */}</Grid.Col>
+          <Grid.Col span={8}>
+            <Stack spacing="lg" mr="lg" my="lg">
+              <TextInput
+                // data-autofocus
+                label="Project Title"
+                placeholder="The name of your project"
+                {...form.getInputProps('projectTitle')}
+              />
+              <TextInput
+                label="Tech Stack"
+                placeholder="React, Next.js, Firebase, etc."
+                {...form.getInputProps('techStack')}
+              />
+              <TextInput
+                label="Project Tags"
+                placeholder="Open Source, Social Network, etc."
+                {...form.getInputProps('projectTags')}
+              />
+              <TextInput
+                label="Live URL"
+                placeholder="https://myproject.com"
+                {...form.getInputProps('liveUrl')}
+              />
+              <TextInput
+                label="Repo URL"
+                placeholder="github.com/myusername/myproject"
+                {...form.getInputProps('repoUrl')}
+              />
+
+              <Checkbox.Group
+                mt='sm'
+                mb='md'
+                value={categoryValue}
+                onChange={setCategoryValue}
+                label="Project Categories"
+                description="Pick the categories that best describe your project"
+              >
+                <Group spacing='xl' mt="md">
+                  <Checkbox value="frontend" label="Frontend" />
+                  <Checkbox value="backend" label="Backend" />
+                  <Checkbox value="fullstack" label="Fullstack" />
+                  <Checkbox value="ai" label="AI" />
+                </Group>
+              </Checkbox.Group>
+
+              <Checkbox
+                mb='sm'
+                label="Visible to the Public"
+                checked={visibleToPublicValue}
+                onChange={(event) => setVisibleToPublicValue(event.currentTarget.checked)}
+              />
+
+              <Checkbox
+                label="Open to Collaboration"
+                checked={openToCollaborationValue}
+                onChange={(event) =>
+                  setOpenToCollaborationValue(event.currentTarget.checked)
+                }
+              />
+              <Group position="right" mt="lg" mr="md">
+                <Button
+                  component="a"
+                  radius="lg"
+                  size="sm"
+                  px={30}
+                  // size={{
+                  //   md: 'lg',
+                  //   sm: 'sm',
+                  // }}
+                  // w={{
+                  //   base: '95%',
+                  //   md: '80%',
+                  //   lg: '60%',
+                  //   sm: '90%',
+                  // }}
+                  // mt={12}
+                  // mb={30}
+                  onClick={handleSaveAsDraft}
+                  // className="mx-auto"
+                  variant="outline"
+                  // onClick={handleSave}
+                  styles={(theme) => ({
+                    root: {
+                      // backgroundColor: theme.colors.green[8],
+                      // width: '40%',
+                      [theme.fn.smallerThan('sm')]: {
+                        // width: '70%',
+                      },
+                      '&:hover': {
+                        // color: theme.colors.white,
+                        color:
+                          theme.colorScheme === 'dark'
+                            ? theme.colors.blue[0]
+                            : theme.colors.blue[0],
+                        backgroundColor:
+                          theme.colorScheme === 'dark'
+                            ? theme.colors.blue[9]
+                            : theme.colors.blue[7],
+                      },
+                    },
+                  })}
+                >
+                  Save Draft
+                </Button>
+                <Button
+                  component="a"
+                  radius="lg"
+                  size="sm"
+                  px={40}
+                  // w='xl'
+                  // w={{
+                  //   base: '95%',
+                  //   md: '80%',
+                  //   lg: '60%',
+                  //   sm: '90%',
+                  // }}
+                  // mt={40}
+                  onClick={handlePublish}
+                  // className="mx-auto"
+                  // onClick={handleSave}
+                  styles={(theme) => ({
+                    root: {
+                      backgroundColor: theme.colors.green[7],
+                      // width: '40%',
+                      [theme.fn.smallerThan('sm')]: {
+                        // width: '70%',
+                      },
+                      '&:hover': {
+                        backgroundColor:
+                          theme.colorScheme === 'dark'
+                            ? theme.colors.green[9]
+                            : theme.colors.green[9],
+                      },
+                    },
+                  })}
+                >
+                  Publish
+                </Button>
+                {/* <Button onClick={open}>Publish</Button> */}
+              </Group>
+            </Stack>
+          </Grid.Col>
+        </Grid>
+      </Modal>
+    </>
+  );
+}
+
+type AiCaseStudyProps = {
+  repoId?: string;
+  repoName?: string;
+};
+
+const AddWithAI = ({ repoId }: AiCaseStudyProps) => {
+  const [projectTitle, setProjectTitle] = useState('GitConnect');
+
+  const [purpose, setPurpose] = useState(
+    'To make a project sharing platform for software developers'
+  );
+
+  const [approach, setApproach] = useState(
+    'Do some research on alternatives, whip up some wireframes, identify tech and then start putting things together'
+  );
+
+  const [technology, setTechnology] = useState(
+    'Next.js, React Firebase, Mantine UI, TypeScript, Github API'
+  );
+
+  const [challenges, setChallenges] = useState(
+    'First time using a lot of the technology, trying to make a scalable platform was challenging as well as integrating with github api - lots of learnings'
+  );
+
+  const [outcomes, setOutcomes] = useState(
+    'Basically ready with the MVP having made good progress, only took 1000 hours more than expected'
+  );
+
+  const [nextSteps, setNextSteps] = useState('Launch the beta and make money');
+
+  const [readme, setReadme] = useState('');
+
+  const [outputLanguage, setOutputLanguage] = useState('HTML');
+
+  const [style, setStyle] = useState('professional, first-person, confident but fun');
+
+  const [length, setLength] = useState('medium');
+
+  const [outPut, setOutPut] = useState<any>('');
+
+  const { userData } = useContext(AuthContext);
+  const userId = userData.userId;
+  const router = useRouter();
+  const { projectName } = router.query;
+
+  const handleSubmit = async () => {
+    const {
+      projectTitle,
+      purpose,
+      approach,
+      challenges,
+      technology,
+      outcomes,
+      nextSteps,
+      style,
+      length,
+    } = form.values;
+
+    const projectDetails = `
+    - Project Title: ${projectTitle}
+    - Purpose: ${purpose}
+    - Approach: ${approach}
+    - Technology: ${technology}
+    - Challenges: ${challenges}
+    - Outcomes: ${outcomes}
+    - Next Steps: ${nextSteps}
+    - README: ${readme}
+    - Output Language: ${outputLanguage}
+    - Style: ${style}
+    - Length: ${length}`;
+
+    // try {
+    //   await axios
+    //     .post('/api/ai/generateProject', {
+    //       userMessage: userMessage,
+    //       systemMessage: systemMessage,
+    //       model: 'gpt-4',
+    //     })
+    //     .then((response) => {
+    //       const sanitizedHTML = DOMPurify.sanitize(response.data, {
+    //         ADD_ATTR: ['target'],
+    //       });
+
+    //       setOutPut(sanitizedHTML as any);
+    //     });
+    // } catch (error) {
+    //   console.error('Failed to generate case study:', error);
+    // }
+  };
+
+  // Adding Mantine form options
+  const form = useForm({
+    initialValues: {
+      projectTitle: `${projectTitle}`,
+      purpose: `${purpose}`,
+      challenges: `${challenges}`,
+      outcomes: `${outcomes}`,
+      approach: `${approach}`,
+      technology: `${technology}`,
+      nextSteps: `${nextSteps}`,
+      style: `${style}`,
+      length: `${length}`,
+    },
+  });
+
+  return (
+    <div>
+      {/* <Space h={70} /> */}
+      <Container mt={70}>
+        <Paper
+          radius="md"
+          withBorder
+          p="lg"
+          sx={(theme) => ({
+            backgroundColor:
+              theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+          })}
+        >
+          <TextInput
+            label="Project Title"
+            placeholder="The name of your project"
+            {...form.getInputProps('projectTitle')}
+          />
+          <Textarea
+            label="Purpose"
+            placeholder="Describe the purposes of the project"
+            {...form.getInputProps('purpose')}
+          />
+          <Textarea
+            label="Approach"
+            placeholder="Describe the approach taken"
+            {...form.getInputProps('approach')}
+          />
+          <TextInput
+            label="Technology"
+            placeholder="Describe the technology used"
+            {...form.getInputProps('technology')}
+          />
+          <Textarea
+            label="Challenges"
+            placeholder="Describe the challenges faced"
+            {...form.getInputProps('challenges')}
+          />
+          <Textarea
+            label="Outcomes"
+            placeholder="Describe the outcomes of the project"
+            {...form.getInputProps('outcomes')}
+          />
+
+          <Textarea
+            label="Next Steps"
+            placeholder="Describe the next steps for the project"
+            {...form.getInputProps('nextSteps')}
+          />
+          <Textarea
+            label="Length of Output"
+            placeholder="How long should the output be? (Short, Medium, Long)"
+            {...form.getInputProps('length')}
+          />
+          <Textarea
+            label="Style of Output"
+            placeholder="What kind of style should the output be? (Casual, Professional, Academic etc)"
+            {...form.getInputProps('style')}
+          />
+
+          <Button variant="filled" fullWidth mt="md" onClick={handleSubmit}>
+            Generate Case Study{' '}
+          </Button>
+          {/* <Button type='submit'></Button> */}
+          {/* </form> */}
+
+          {/* <EditableCaseStudy aiGeneratedContent={outPut} /> */}
+          {/* <div dangerouslySetInnerHTML={{ __html: outPut }} /> */}
+        </Paper>
+      </Container>
+    </div>
+  );
+};
+
+// export default AddWithAI;
+
+function Demo() {
+  const [opened, { open, close }] = useDisclosure(false);
+
+  return (
+    <>
+      <Modal.Root opened={opened} onClose={close}>
+        <Modal.Overlay />
+        <Modal.Content>
+          <Modal.Header>
+            <Modal.Title>Modal title</Modal.Title>
+            <Modal.CloseButton />
+          </Modal.Header>
+          <Modal.Body>Modal content</Modal.Body>
+        </Modal.Content>
+      </Modal.Root>
+
+      <Group position="center">
+        <Button onClick={open}>Open modal</Button>
+      </Group>
+    </>
+  );
+}
+
+export default ProjectSettingsModal;
