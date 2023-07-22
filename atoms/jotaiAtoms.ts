67b18cbfb00ef1e2ad4777635666a79392772c9e
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-07-22 15:55:23 +1000

    Several issues resolved, still some minor out of sync behaviour on multiple edit changes

--- atoms/jotaiAtoms.ts
+++ atoms/jotaiAtoms.ts
@@ -1,5 +1,18 @@
 import { atom } from 'jotai'
+// import { atomWithHash } from 'jotai-location'
 import { RepoDataFull } from '../types/repos'
 
 export const projectDataAtom = atom<RepoDataFull>({} as RepoDataFull)
 export const textEditorAtom = atom<string | undefined>('');
+
+const pageAtom = atomWithHash('page', 1, {
+  replaceState: true,
+  subscribe: (callback) => {
+    Router.events.on('routeChangeComplete', callback)
+    window.addEventListener('hashchange', callback)
+    return () => {
+      Router.events.off('routeChangeComplete', callback)
+      window.removeEventListener('hashchange', callback)
+    }
+  },
+})
