df29cd4d845f110b55859c65af0c2872e6d36884
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-09-08 21:17:58 +1000

    updated profile routes - cleaned up code - to test

--- pages/api/portfolio/getUserProfile.ts
+++ pages/api/portfolio/getUserProfile.ts
@@ -2,14 +2,11 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getProfileDataWithUsernameLowercase } from '@/lib/profiles';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  console.log('getUserProfile hit')
   const username = req.query.username as string;
-  console.log('username in getUserProfile.ts:', username);
   try {
     const data = await getProfileDataWithUsernameLowercase(username);
-    // console.log('data in getUserProfile.ts:', data);
     res.status(200).json(data?.[0]?.docData || null);
-  } catch (error: any) {
+  } catch (error) {
     res.status(500).json({ error: (error as Error).message });
   }
 }
