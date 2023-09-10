5a91ee5576b034a4e58ea8c7d78fac4b7ca56bce
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-09-10 12:49:13 +1000

    Cleaned up many files to try an resolve build errors

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,9 +1,9 @@
 import React, { useContext } from "react"
 import { AuthContext } from "../context/AuthContext"
 import { useRouter } from "next/router"
-import GithubLogin from "../components//LoginPages/GithubLogin"
-import EmailLogin from "../components/LoginPages/EmailLogin"
-import GoogleLogin from "../components//LoginPages/GoogleLogin"
+// import GithubLogin from "../components//LoginPages/GithubLogin"
+// import EmailLogin from "../components/LoginPages/EmailLogin"
+// import GoogleLogin from "../components//LoginPages/GoogleLogin"
 import { LoginPage } from "../components/LoginPages/Login"
 // import Mixpanel from "mixpanel"
 
