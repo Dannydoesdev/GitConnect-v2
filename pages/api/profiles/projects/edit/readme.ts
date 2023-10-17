24071232ecbb42752e281ce13ab5edeadabe0740
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-10-17 16:19:53 +1100

    Resolved weird Github readme import bug, probably some updates to to portfolio page in this one too

--- pages/api/profiles/projects/edit/readme.ts
+++ pages/api/profiles/projects/edit/readme.ts
@@ -1,18 +1,15 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { Octokit } from "@octokit/rest";
-import { Endpoints } from "@octokit/types";
-import DOMPurify from 'dompurify';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Octokit } from '@octokit/rest';
+import { Endpoints } from '@octokit/types';
+import DOMPurify from 'isomorphic-dompurify';
 
 type RequestData = {
-  repo: string,
-  owner: string
-}
+  repo: string;
+  owner: string;
+};
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "GET") {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
     // Handle GET request
     // console.log('request received')
     // const { repo, owner }: ResponseData = req.query;
@@ -23,17 +20,16 @@ export default async function handler(
 
     // TODO: fix type issues with strict type here
     if (repoReq && ownerReq) {
-
-      const repo = repoReq.toString()
-      const owner = ownerReq.toString()
+      const repo = repoReq.toString();
+      const owner = ownerReq.toString();
       // rest of the code
       const octokit = new Octokit();
       // const owner = "dannydoesdev"; // change this to your desired owner
       // const repo = "gitconnect"; // change this to your desired repo
       // console.log(`GET /repos/${owner}/${repo}/readme`)
       // Get README.md file conten
-      type GetReadmeResponse = Endpoints["GET /repos/{owner}/{repo}/readme"]["response"];
-    
+      type GetReadmeResponse = Endpoints['GET /repos/{owner}/{repo}/readme']['response'];
+
       // TODO: fix type issues with strict type here
 
       // let readme: GetReadmeResponse;
@@ -41,43 +37,40 @@ export default async function handler(
       let readme: any;
 
       // NOTE - removing mediaType will return all url links to the different types, could be useful to store these seperately for later
-  
+
       try {
         readme = await octokit.repos.getReadme({
           owner,
           repo,
           // path: `profiles/${id}.md`, // change this to your desired path
           mediaType: {
-            format: "html", // change this to your desired format
+            format: 'html', // change this to your desired format
           },
-        })
-          // .then((response) => {
-          //   console.log('\n\n\nfull readme response\n\n\n')
-          // console.log(readme)
-        
-          const readmeData = readme.data;
-          // console.log(`\n\n\nreadme.data\n\n\n`)
-          // console.log(readmeData)
-
-        
-          // const sanitizedHTML = DOMPurify.sanitize(readmeData);
-          // console.log('\n\n\nsanitized\n\n\n')
-  
-          // console.log(sanitizedHTML)
-        
+        });
+        // .then((response) => {
+        //   console.log('\n\n\nfull readme response\n\n\n')
         // console.log(readme)
-        res.setHeader("Content-Type", "text/html");
+
+        const readmeData = readme.data;
+        // console.log(readmeData);
+        // console.log(`\n\n\nreadme.data\n\n\n`)
+        // console.log(readmeData)
+
+        // const sanitizedHTML = DOMPurify.sanitize(readmeData);
+        // console.log('\n\n\nsanitized\n\n\n')
+
+        // console.log(sanitizedHTML)
+
+        // console.log(readme)
+        res.setHeader('Content-Type', 'text/html');
         // console.log(readme.data.content)
 
         res.status(200).send(readmeData);
 
         // })
         // Send back file content as HTML
-     
-
-
       } catch (error) {
-        res.status(404).json({ message: "File not found" });
+        res.status(404).json({ message: 'File not found' });
         return;
       }
       // res.send({message: 'hi'})
@@ -86,21 +79,15 @@ export default async function handler(
       // Send back file content as plain text
       // res.setHeader("Content-Type", "text/plain");
       // res.status(200).send(Buffer.from(readme.data.content, "base64").toString());
-
-
-
     } else {
       //   // Return 405 Method Not Allowed
       res.status(405).end();
     }
-  }else {
+  } else {
     //   // Return 405 Method Not Allowed
-    res.status(404).json({ message: "No owner or repo received" });
+    res.status(404).json({ message: 'No owner or repo received' });
   }
-
-
 }
-
 
 // import React, { useState, useEffect } from 'react';
 // import { Octokit, App } from 'octokit';
@@ -136,3 +123,40 @@ export default async function handler(
 // };
 
 // export default Readme;
+
+
+    // const readmeData = readme.data;
+
+      //   DOMPurify.addHook('uponSanitizeElement', (currentNode: Element, data) => {
+      //     if (data.tagName === 'a' && currentNode.classList.contains('heading-link')) {
+      //       // Create a text node with the anchor's content
+      //       const textContent = currentNode.textContent || ''; // Fallback to empty string if null
+      //       const textNode = document.createTextNode(textContent);
+
+      //       // Replace the anchor with the text node
+      //       const parentNode = currentNode.parentNode;
+      //       if (parentNode) {
+      //         parentNode.replaceChild(textNode, currentNode);
+      //       }
+      //     }
+      //   });
+
+      //   const sanitizedHTML = DOMPurify.sanitize(readmeData, { ADD_ATTR: ['target'] });
+      //   // console.log(readme)
+      //   console.log('server sanitized html', sanitizedHTML)
+      //   res.setHeader('Content-Type', 'text/html');
+
+      //   res.status(200).send(sanitizedHTML);
+
+
+      // } catch (error) {
+      //   res.status(404).json({ message: 'File not found' });
+      //   return;
+      // }
+      // finally {
+      //   res.setHeader('Content-Type', 'text/html');
+      //   res.status(200).send(sanitizedHTML);
+      // }
+
+
+
