a836565e216acca8c1dfe68420e1c0c24bdf87a6
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-11-12 17:07:26 +1100

    resolve issues with openai packages

--- lib/generateProject.ts
+++ lib/generateProject.ts
@@ -1,19 +1,28 @@
-import { Configuration, OpenAIApi } from 'openai';
+import OpenAI from 'openai';
 
 export async function generateProjectWithSystemAndUserMessage( userMessage: string, systemMessage: string, model: string) {
 
   // console.log('userMessage', userMessage);
   // console.log('systemMessage', systemMessage);
   // console.log('model', model);
-  
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+  const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+//   const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// // });
+  // const openai = new OpenAI(process.env.OPENAI_API_KEY);
+// const configuration = new Configuration({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
-const openai = new OpenAIApi(configuration);
+// const openai = new OpenAIApi(configuration);
 
   try {
-    const completion = await openai.createChatCompletion({
+    
+    const completion = await openai.chat.completions.create({
+    // const completion = await openai.completions.create({
+    // const completion = await openai.createCompletion({
+
+    // const completion = await openai.createChatCompletion({
       // model: 'gpt-3.5-turbo',
       // model: 'gpt-4',
       model: model,
@@ -26,7 +35,8 @@ const openai = new OpenAIApi(configuration);
     }
     );
     // console.log(completion.data.choices[0])
-    const response = completion.data?.choices[0]?.message?.content;
+    const response = completion.choices[0]?.message?.content;
+    // const response = completion.data?.choices[0]?.message?.content;
     // console.log('response', response);
     return response;
   } catch (error) {
