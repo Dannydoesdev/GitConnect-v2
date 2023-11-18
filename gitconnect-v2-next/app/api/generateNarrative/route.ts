ddfb9f0ec05ed85d867fa4d4c02e50b3516a2405
Daniel McGee <daniel.t.mcgee@gmail.com>
2023-11-18 20:25:15 +1100

    AI Implementation improved further, check the reloading on first response issue

--- gitconnect-v2-next/app/api/generateNarrative/route.ts
+++ gitconnect-v2-next/app/api/generateNarrative/route.ts
@@ -6,65 +6,70 @@ export const runtime = 'edge';
 
 const systemMessage = `You are an intelligent assistant that crafts personalised software project case studies for the users software development portfolio. Given the user's preferences for length, style, and formatting, create an engaging first-person narrative from the provided project details. Include sections such as Introduction, Purpose, Approach, Technology Used, Challenges, Outcomes, and Next Steps. Adapt the content and structure as per unique elements in the user's inputs, and introduce new headings when necessary. Format the output with accurate appropriate formatting in complete HTML language syntax.`;
 
+const systemMessageTwo = `You are an AI skilled in crafting compelling narratives for software development projects. Using the details provided, generate an engaging, well-structured narrative in HTML format. The narrative should highlight key aspects such as the project's purpose, challenges, technologies used, and outcomes. Incorporate headings, paragraphs, and lists to enhance readability. Aim to reflect the user's unique experience and insights, maintaining their voice throughout. Ensure the final output is a cohesive, professional piece, suitable for showcasing in a software developer's portfolio.`;
+
+const systemMessageThree = `You are an AI assisting in writing a narrative for a software development project. Craft a first-person account that directly delves into the project's journey, avoiding formal introductions like 'Welcome to the narrative.' Focus on the project's purpose, challenges, technologies used, and outcomes, portraying it as a developer's personal recount. Use HTML format with clear headings, paragraphs, and lists. Keep the tone professional yet personal, reflecting the developer's voice and experience. Ensure the narrative is engaging, cohesive, and suitable for a developer's portfolio.`;
+
+const systemMessageFour = "You are an AI assisting in writing a narrative for a software development project. Craft a story from the first-person perspective that directly delves into the project's journey, avoiding formal introductions like 'Welcome to the narrative.' Focus on the project's purpose, challenges, technologies used, and outcomes, portraying it as a developer's personal recount. Format your respone in complete HTML language syntax with clear headings, paragraphs, and lists. Do NOT include the language and triple backticks (```html ) at the beginning and end of your response. Keep the tone professional yet personal, reflecting the developer's voice and experience. Ensure the narrative is engaging, cohesive, and suitable for a developer's portfolio.";
+
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY!,
 });
 
 export async function POST(req: Request, res: NextApiResponse) {
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-//   if (req.method !== 'POST') {
-//     return res.status(405).end(`Method ${req.method} Not Allowed`);
-//   }
-
-  // const { prompt } = await req.json();
-
-  // console.log('POST request received');
-  // // Input validation
-  // const { title, description, technologies, role, duration } = req.body;
-  // if (!title || !description) { // Simple validation, expand according to requirements
-  //   return res.status(400).json({ error: 'Missing required fields' });
-  // }
   const { prompt } = await req.json();
 
   try {
-    // const projectData = req.body;
-    // console.log('projectData: ', projectData);
-   
-    // const prompt = formatPrompt(projectData);
-    // console.log('prompt: ', prompt);
-
     // Send the prompt to the OpenAI API for a streamed response
     const response = await openai.chat.completions.create({
-      model: 'gpt-4-1106-preview',
+      // model: 'gpt-4-1106-preview',
+      model: 'gpt-3.5-turbo-1106',
       messages: [
         // { role: 'system', content: 'Start a new project narrative.' },
-        { role: 'system', content: systemMessage },
+        { role: 'system', content: systemMessageFour },
         { role: 'user', content: prompt },
       ],
-      max_tokens: 700,
+      max_tokens: 1000,
       stream: true,
     });
     // console.log('response: ', response)
     // Stream the response back to the client
     const stream = OpenAIStream(response);
     return new StreamingTextResponse(stream);
-
-    // // Check if response is successful
-    // if (response.data) {
-    //   // Send back the narrative
-    //   res.status(200).json({ narrative: response.data.choices[0].text });
-    // } else {
-    //   throw new Error('Invalid response from the OpenAI API.');
-    // }
-    // res.status(200).json({ narrative });
   } catch (error) {
     console.error(error);
     res.status(500).end('Internal Server Error');
   }
-
 }
 
+// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).end(`Method ${req.method} Not Allowed`);
+//   }
+
+// const { prompt } = await req.json();
+
+// console.log('POST request received');
+// // Input validation
+// const { title, description, technologies, role, duration } = req.body;
+// if (!title || !description) { // Simple validation, expand according to requirements
+//   return res.status(400).json({ error: 'Missing required fields' });
+// }
+// const projectData = req.body;
+// console.log('projectData: ', projectData);
+
+// const prompt = formatPrompt(projectData);
+// console.log('prompt: ', prompt);
+
+// // Check if response is successful
+// if (response.data) {
+//   // Send back the narrative
+//   res.status(200).json({ narrative: response.data.choices[0].text });
+// } else {
+//   throw new Error('Invalid response from the OpenAI API.');
+// }
+// res.status(200).json({ narrative });
 // function formatPrompt(projectData: any): string {
 //   // Format your project data into a coherent prompt for GPT-4
 //   // Example formatting (customize this as needed)
@@ -74,8 +79,6 @@ export async function POST(req: Request, res: NextApiResponse) {
 //     projectData.role
 //   }\nDuration: ${projectData.duration}\n...`; // Continue with other fields
 // }
-
-
 
 // async function generateNarrative(projectData: any) {
 //   const openai = new OpenAI({
--- gitconnect-v2-next/pages/ai/createProject.tsx
+++ gitconnect-v2-next/pages/ai/createProject.tsx
@@ -1,21 +1,25 @@
 import { useEffect, useState } from 'react';
+import { ProjectFormProvider, useProjectForm } from '@/context/formContext';
+import {
+  Button,
+  Container,
+  Drawer,
+  Group,
+  Stack,
+  Textarea,
+  TextInput,
+} from '@mantine/core';
 import { useForm } from '@mantine/form';
+import { useDisclosure } from '@mantine/hooks';
+import { useCompletion } from 'ai/react';
+import { useDebouncedCallback } from 'use-debounce';
 import { CreateProjectStepper } from '@/components/ai/attemptTwo/CreateProjectStepper';
 import { NarrativeEditor } from '@/components/ai/attemptTwo/NarrativeEditor';
 import { StepPanel } from '@/components/ai/attemptTwo/StepPanel';
-import { ProjectFormProvider, useProjectForm } from '@/context/formContext';
-import { TextInput, Textarea, Group, Button, Container, Stack, Drawer } from '@mantine/core';
-import { useCompletion } from 'ai/react';
-import { useDebouncedCallback } from 'use-debounce';
-import { useDisclosure } from '@mantine/hooks';
- 
 
 const CreateProjectPage = () => {
   const [currentStep, setCurrentStep] = useState(0);
-  const [narrativeContent, setNarrativeContent] = useState('');
   const [opened, { open, close }] = useDisclosure(false);
-
-  // const { completion, input, handleInputChange, handleSubmit } = useCompletion();
   const {
     complete,
     completion,
@@ -26,7 +30,7 @@ const CreateProjectPage = () => {
     handleSubmit,
   } = useCompletion({
     api: '/api/generateNarrative',
-    onResponse: res => {
+    onResponse: (res) => {
       // trigger something when the response starts streaming in
       // e.g. if the user is rate limited, you can show a toast
       // if (res.status === 429) {
@@ -40,11 +44,14 @@ const CreateProjectPage = () => {
     },
   });
 
+  // ...inside your CreateProjectPage or similar component
+  // const [narrativeContent, setNarrativeContent] = useState(''); // State to hold the content
+
   //  Can be used for realtime streaming
   // const handleInputChange = useDebouncedCallback(e => {
   //   complete(e.target.value);
   // }, 500);
-  
+
   // The actual number of steps should be the number of StepPanel components you have
   const totalSteps = 6;
 
@@ -65,6 +72,156 @@ const CreateProjectPage = () => {
   //   },
   // });
 
+  const form = useProjectForm({
+    initialValues: {
+      title: 'GitConnect',
+      description: '',
+      technologies: 'React, typescript, firestore, nextjs, mantine UI',
+      role: 'Founder, designer and lead dev',
+      duration: '',
+      purpose: 'Create a portfolio platform for devs',
+      challenges:
+        'Learning new technologies, turning it into a startup, launching, finding users',
+      solutions:
+        'Github API to import public projects, lots of helper functions, project pages for each project, ai generation for project assistance',
+      learnings:
+        'Heaps - how to start a company, how not to start a company, honestly how to code altogether',
+      outcome:
+        'Finally launched publicly after 450+ commits, now just trying to find users!',
+      images: [],
+      liveProjectURL: '',
+    },
+  });
+
+  function formatPrompt(projectData: any): string {
+    return `Project Title: ${projectData.title}\n
+  Users Role in Project: ${projectData.role}\n
+  Project Technologies: ${projectData.technologies}\n
+  Purpose of the project: ${projectData.purpose}\n
+  Challenges faced during the project: ${projectData.challenges}\n
+  Solutions implemented in the project: ${projectData.solutions}\n
+  Learnings from the project: ${projectData.learnings}\n
+  Outcome of the project: ${projectData.outcome}`; // Continue with other fields
+  }
+
+  function formatPromptTwo(projectData: any): string {
+    let prompt = `Project Title: ${projectData.title}\nUsers role in the project: ${projectData.role}\nTechnologies used in the project: ${projectData.technologies}\n`;
+
+    if (projectData.purpose) {
+      prompt += `Purpose of the project: ${projectData.purpose}\n`;
+    }
+    if (projectData.challenges) {
+      prompt += `Challenges faced during the project: ${projectData.challenges}\n`;
+    }
+    if (projectData.solutions) {
+      prompt += ` Solutions implemented in the project: ${projectData.solutions}\n`;
+    }
+    if (projectData.learnings) {
+      prompt += `Learnings from the project: ${projectData.learnings}\n`;
+    }
+    if (projectData.outcome) {
+      prompt += `Outcome of the project: ${projectData.outcome}\n`;
+    }
+
+    // Include other fields similarly if they are not empty
+
+    return prompt;
+  }
+
+  // This function is called when the user proceeds to the next step or requests a narrative
+  const handleNextStep = async () => {
+    if (currentStep === totalSteps - 1) {
+      const prompt = formatPrompt(form.values);
+      console.log('prompt:', prompt);
+      open();
+      try {
+        complete(prompt);
+        // const narrative: any = await generateNarrative(form.values);
+        // setNarrativeContent(completion);
+        // console.log('completion:')
+        // console.log(completion)
+        // setNarrativeContent(completion);
+
+        // Here you might want to navigate to a "Success" page or show the narrative
+      } catch (error) {
+        // Handle errors, such as updating state to show an error message
+        console.log('error:', error);
+      }
+    } else {
+      setCurrentStep((current) => current + 1);
+    }
+  };
+
+  const handleBackStep = () => {
+    setCurrentStep((current) => current - 1);
+  };
+
+  // // This function should be called to stream data from GPT-4 and update narrativeContent
+  // const streamDataToEditor = async () => {
+  //   // Replace with actual streaming logic
+  //   const streamedContent = await getStreamedContentFromGPT4();
+  //   setNarrativeContent(streamedContent);
+  // };
+
+  return (
+    <>
+      <ProjectFormProvider form={form}>
+        <div className="create-project-container mt-20">
+          <div className="panel-left">
+            {/* The stepper component that manages which step the user is on */}
+            <CreateProjectStepper
+              totalSteps={totalSteps}
+              currentStep={currentStep}
+              setCurrentStep={setCurrentStep}
+            >
+              {/* Here you render StepPanel components for each step */}
+              <StepPanel step={0} title="Basic Info" />
+              {/* <StepPanel step={1} title="Tech & Role" /> */}
+              <StepPanel step={1} title="Purpose" />
+              <StepPanel step={2} title="Challenges" />
+              <StepPanel step={3} title="Solutions" />
+              <StepPanel step={4} title="Learnings" />
+              <StepPanel step={5} title="Outcome" />
+            </CreateProjectStepper>
+
+            <Group position="center" mt="xl">
+              <Button
+                onClick={handleBackStep}
+                variant="default"
+                disabled={currentStep === 0}
+              >
+                Back
+              </Button>
+              <Button onClick={handleNextStep}>
+                {currentStep === totalSteps - 1 ? 'Submit' : 'Next step'}
+              </Button>
+            </Group>
+          </div>
+          <div className="panel-right">
+            {/* The component that displays the narrative generated by the AI */}
+            {/* <NarrativeEditor content={narrativeContent} /> */}
+            {/* <NarrativeEditor content={completion} /> */}
+          </div>
+        </div>
+      </ProjectFormProvider>
+      <>
+        <Drawer opened={opened} onClose={close} title="Project narrative generation">
+          <div className="panel-right">
+            <NarrativeEditor
+              generatedContent={completion} // Directly pass the completion state to the editor
+            />
+          </div>
+          {/* Drawer content */}
+          {/* <div dangerouslySetInnerHTML={{ __html: completion }}></div> */}
+          {/* <p>{completion}</p> */}
+          {/* <NarrativeEditor content={completion} /> */}
+        </Drawer>
+      </>
+    </>
+  );
+};
+
+export default CreateProjectPage;
 
 // Title:
 // GitConnect
@@ -88,190 +245,72 @@ const CreateProjectPage = () => {
 // heaps - how to start a company, how not to start a company, honestly how to code altogether
 
 // Outcome:
-  // finally launched publicly after 450+ commits, now just trying to find users!
-  
-  const form = useProjectForm({
-    initialValues: {
-      title: 'GitConnect',
-      description: '',
-      technologies: 'React, typescript, firestore, nextjs, mantine UI',
-      role: 'Founder, designer and lead dev',
-      duration: '',
-      purpose: 'Create a portfolio platform for devs',
-      challenges: 'Learning new technologies, turning it into a startup, launching, finding users',
-      solutions: 'Github API to import public projects, lots of helper functions, project pages for each project, ai generation for project assistance',
-      learnings: 'Heaps - how to start a company, how not to start a company, honestly how to code altogether',
-      outcome: 'Finally launched publicly after 450+ commits, now just trying to find users!',
-      images: [],
-      liveProjectURL: '',
-    },
-  });
+// finally launched publicly after 450+ commits, now just trying to find users!
+// const submitProjectData = async (projectData: any) => {
+//   // Replace with the actual API endpoint and request logic
+//   // Mocked response for illustration
+//   const response = await fetch('/api/generateNarrative', {
+//     method: 'POST',
+//     body: JSON.stringify(projectData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
 
-  // useEffect(() => {
-  //   console.log(form.values);
-  // }, [form.values]);
+//   const narrative = await response.json();
+//   return narrative;
+// };
 
-  // This function makes an API call to submit the project data and receives the narrative
-  // const submitProjectData = async (projectData: any) => {
-  //   const response = await fetch('/api/generateNarrative', {
-  //     method: 'POST',
-  //     body: JSON.stringify(projectData),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
+// const handleNextStep = async () => {
+//   if (currentStep === totalSteps - 1) {
+//     await generateNarrative(form.values);
+//   } else {
+//     setCurrentStep((current) => current + 1);
+//   }
+// };
+//  {/* </Stack> */}
+//             {/* </Container> */}
+//          {/* <Container mt='md'> */}
+//       {/* <Stack spacing='md'> */}
+//    {/* <Group position="right">
+//         <Button onClick={open}>Open Drawer</Button>
+//       </Group> */}
+// useEffect(() => {
+//   console.log(form.values);
+// }, [form.values]);
 
-  //   if (!response.ok) {
-  //     // Handle errors here, such as displaying a message to the user
-  //     throw new Error('Failed to generate narrative');
-  //   }
+// This function makes an API call to submit the project data and receives the narrative
+// const submitProjectData = async (projectData: any) => {
+//   const response = await fetch('/api/generateNarrative', {
+//     method: 'POST',
+//     body: JSON.stringify(projectData),
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
 
-  //   const narrative = await response.json();
-  //   return narrative;
-  // };
-  // const generateNarrative = async (projectData: any) => {
-  //   // This function would call your backend API which in turn communicates with OpenAI's GPT
-  //   // For now, we'll mock this to update the narrative state
-  //   const generatedNarrative = await submitProjectData(projectData);
-  //   setNarrativeContent(generatedNarrative);
-  // };
+//   if (!response.ok) {
+//     // Handle errors here, such as displaying a message to the user
+//     throw new Error('Failed to generate narrative');
+//   }
 
-  function formatPrompt(projectData: any): string {
-
-  // Format your project data into a coherent prompt for GPT-4
-  // Example formatting (customize this as needed)
-  return `Project Title: ${projectData.title}\n
-  Users Role in Project: ${projectData.role}\n
-  Project Technologies: ${projectData.technologies}\n
-  Purpose of the project: ${projectData.purpose}\n
-  Challenges faced during the project: ${projectData.challenges}\n
-  Learnings from the project: ${projectData.learnings}\n
-  Outcome of the project: ${projectData.outcome}`; // Continue with other fields
-}
-
-  // This function is called when the user proceeds to the next step or requests a narrative
-  const handleNextStep = async () => {
-    if (currentStep === totalSteps - 1) {
-      const prompt = formatPrompt(form.values);
-      console.log('prompt:', prompt)
-      open();
-      try {
-        complete(prompt)
-        // const narrative: any = await generateNarrative(form.values);
-        // setNarrativeContent(completion);
-        // console.log('completion:')
-        // console.log(completion)
-        // Here you might want to navigate to a "Success" page or show the narrative
-      } catch (error) {
-        // Handle errors, such as updating state to show an error message
-        console.log('error:', error)
-      }
-    } else {
-      setCurrentStep((current) => current + 1);
-    }
-  };
-
-  // const submitProjectData = async (projectData: any) => {
-  //   // Replace with the actual API endpoint and request logic
-  //   // Mocked response for illustration
-  //   const response = await fetch('/api/generateNarrative', {
-  //     method: 'POST',
-  //     body: JSON.stringify(projectData),
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //     },
-  //   });
-  
-  //   const narrative = await response.json();
-  //   return narrative;
-  // };
-
-  // const handleNextStep = async () => {
-  //   if (currentStep === totalSteps - 1) {
-  //     await generateNarrative(form.values);
-  //   } else {
-  //     setCurrentStep((current) => current + 1);
-  //   }
-  // };
-
-  const handleBackStep = () => {
-    setCurrentStep((current) => current - 1);
-  };
-
-
-
-  return (
-    <>
-  
-    <ProjectFormProvider form={form}>
-      <div className="create-project-container mt-20">
-        <div className="panel-left">
-          {/* The stepper component that manages which step the user is on */}
-          <CreateProjectStepper
-            totalSteps={totalSteps}
-            currentStep={currentStep}
-            setCurrentStep={setCurrentStep}
-          >
-             {/* <Container mt='md'> */}
-      {/* <Stack spacing='md'> */}
-            {/* Here you render StepPanel components for each step */}
-            <StepPanel step={0} title="Basic Info" />
-            {/* <StepPanel step={1} title="Tech & Role" /> */}
-            <StepPanel step={1} title="Purpose" />
-            <StepPanel step={2} title="Challenges" />
-            <StepPanel step={3} title="Solutions" />
-            <StepPanel step={4} title="Learnings" />
-                <StepPanel step={5} title="Outcome" />
-                
-              {/* </Stack> */}
-            {/* </Container> */}
-              </CreateProjectStepper>
-              
-            
-  
-          <Group position="center" mt="xl">
-            <Button onClick={handleBackStep} variant='default' disabled={currentStep === 0}>
-              Back
-            </Button>
-            <Button onClick={handleNextStep}>
-              {currentStep === totalSteps - 1 ? 'Submit' : 'Next step'}
-            </Button>
-          </Group>
-        </div>
-        <div className="panel-right">
-          {/* The component that displays the narrative generated by the AI */}
-          {/* <NarrativeEditor content={narrativeContent} /> */}
-          {/* <NarrativeEditor content={completion} /> */}
-
-        </div>
-      </div>
-      </ProjectFormProvider>
-      <>
-      <Drawer opened={opened} onClose={close} title="Project narrative generation">
-          {/* Drawer content */}
-          <div dangerouslySetInnerHTML={{ __html: completion }}></div>
-          <p>{completion}</p>
-          {/* <NarrativeEditor content={completion} /> */}
-      </Drawer>
-
-      {/* <Group position="right">
-        <Button onClick={open}>Open Drawer</Button>
-      </Group> */}
-    </>
-    </>
-  );
-};
-
-export default CreateProjectPage;
-
+//   const narrative = await response.json();
+//   return narrative;
+// };
+// const generateNarrative = async (projectData: any) => {
+//   // This function would call your backend API which in turn communicates with OpenAI's GPT
+//   // For now, we'll mock this to update the narrative state
+//   const generatedNarrative = await submitProjectData(projectData);
+//   setNarrativeContent(generatedNarrative);
+// };
 
 //   return (
 //     <ProjectFormProvider form={form}>
 //       <div className="create-project-container mt-20">
 //         <div className="panel-left">
-//           <CreateProjectStepper 
-//             totalSteps={totalSteps} 
-//             currentStep={currentStep} 
+//           <CreateProjectStepper
+//             totalSteps={totalSteps}
+//             currentStep={currentStep}
 //             setCurrentStep={setCurrentStep}
 //           >
 //              {/* Pass StepPanel components as children with form fields */}
@@ -288,7 +327,7 @@ export default CreateProjectPage;
 //               />
 //             </StepPanel>
 //             <StepPanel step={1} title="Technologies and Role">
-              
+
 //               {/* Include fields for technologies and role here */}
 //             </StepPanel>
 //             <StepPanel step={2} title="Purpose">
@@ -314,9 +353,9 @@ export default CreateProjectPage;
 //   );
 // };
 
-
-
-            {/* Pass StepPanel components as children */}
+{
+  /* Pass StepPanel components as children */
+}
 //             <StepPanel step={0} title="Title and Description" />
 //             <StepPanel step={1} title="Technologies and Role" />
 //             <StepPanel step={2} title="Purpose" />
@@ -339,27 +378,26 @@ export default CreateProjectPage;
 
 // export default CreateProjectPage;
 
+// const [projectData, setProjectData] = useState({
+//   title: '',
+//   description: '',
+//   // ... other fields
+// });
+// const [narrative, setNarrative] = useState('');
 
-  // const [projectData, setProjectData] = useState({
-  //   title: '',
-  //   description: '',
-  //   // ... other fields
-  // });
-  // const [narrative, setNarrative] = useState('');
-
-  // const handleDataChange = (newData) => {
-  //   setProjectData({ ...projectData, ...newData });
-  // };
-
-  // const handleStepChange = (stepNumber) => {
-  //   setCurrentStep(stepNumber);
+// const handleDataChange = (newData) => {
+//   setProjectData({ ...projectData, ...newData });
 // };
-  
-  // const handleSubmit = async () => {
-  //   // This function will be responsible for submitting the final data to the backend
-  //   // and could also handle the narrative generation
-  //   await generateNarrative();
-  //   // Additional submit logic...
-  // };
 
-  // ... onSubmit logic, API calls, etc.
+// const handleStepChange = (stepNumber) => {
+//   setCurrentStep(stepNumber);
+// };
+
+// const handleSubmit = async () => {
+//   // This function will be responsible for submitting the final data to the backend
+//   // and could also handle the narrative generation
+//   await generateNarrative();
+//   // Additional submit logic...
+// };
+
+// ... onSubmit logic, API calls, etc.
