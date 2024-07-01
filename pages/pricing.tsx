dcacd45ae0bb6f2ff2064438c7f2bdf7dc2ebcf7
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-01 08:05:47 +0200

    Major cleanup of older files / comments etc preceeding weaviate integration

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -36,21 +36,6 @@ interface Pricing {
 // Define the possible keys for frequency values
 type Frequency = 'monthly' | 'annually';
 
-// // Define the structure of your tier object
-// type Tier = {
-//   id: string;
-//   name: string;
-//   price: Record<Frequency, string>;
-//   discountedPrice?: Record<Frequency, string>; // Make it optional if not all tiers have discounted prices
-//   // ... other properties
-// };
-
-// Your pricing object should match the Tier type
-// const pricing = {
-//   tiers: Array<Tier>, // Populate with your tiers data
-//   // ... other properties
-// };
-
 type pricingTier = {
   name: string;
   id: string;
@@ -292,18 +277,15 @@ export default function PricingPage() {
   }, [userData]);
 
   const upgradeToPremiumMonthly = async () => {
-
     const priceId = 'price_1O8cptCT5BNNo8lFuDcGOAcM';
     const checkoutUrl = await getCheckoutUrl(app, priceId);
     Router.push(checkoutUrl);
   };
   const upgradeToPremiumAnnual = async () => {
-
-     const priceId = 'price_1O8cq0CT5BNNo8lFWd3e5TYy';
+    const priceId = 'price_1O8cq0CT5BNNo8lFWd3e5TYy';
     const checkoutUrl = await getCheckoutUrl(app, priceId);
     Router.push(checkoutUrl);
   };
-
 
   // const upgradeToPremiumMonthly = async () => {
   //   const priceId = 'price_1O80UbCT5BNNo8lF98l4hlov';
@@ -489,13 +471,16 @@ export default function PricingPage() {
                   {tier.name.includes('Pro') ? (
                     <>
                       <span className="text-4xl font-bold tracking-tight text-gray-300 line-through">
-                        {/* ${tier.price[frequency.value]}{' '} */}
-                        ${tier.price[frequency.value as 'monthly' | 'annually']}{' '}
-
+                        {/* ${tier.price[frequency.value]}{' '} */}$
+                        {tier.price[frequency.value as 'monthly' | 'annually']}{' '}
                         {/* Assuming this is the original price */}
                       </span>
                       <span className="ml-2 text-4xl font-bold tracking-tight text-white">
-                      ${tier.discountedPrice && tier.discountedPrice[frequency.value as 'monthly' | 'annually']}{' '} 
+                        $
+                        {tier.discountedPrice &&
+                          tier.discountedPrice[
+                            frequency.value as 'monthly' | 'annually'
+                          ]}{' '}
                         {/* ${tier.discountedPrice[frequency.value]}{' '} */}
                         {/* You will need to add the discounted price in your tier data */}
                       </span>
@@ -508,7 +493,6 @@ export default function PricingPage() {
                       <span className="text-4xl font-bold tracking-tight text-white">
                         {/* {tier.price[frequency.value]} */}
                         {tier.price[frequency.value as 'monthly' | 'annually']}{' '}
-
                       </span>
                       <span className="text-sm font-semibold leading-6 text-gray-300">
                         {frequency.priceSuffix}
@@ -635,314 +619,7 @@ export default function PricingPage() {
             ))}
           </div>
         </div>
-        {/* ORIGINAL PRICING SECTION */}
-
-        {/* <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-            {pricing.tiers.map((tier) => (
-              <div
-                key={tier.id}
-                className={classNames(
-                  tier.mostPopular
-                    ? 'bg-white/5 ring-2 ring-indigo-500'
-                    : 'ring-1 ring-white/10',
-                  'rounded-3xl p-8 xl:p-10'
-                )}
-              >
-                <div className="flex items-center justify-between gap-x-4">
-                  <h2 id={tier.id} className="text-lg font-semibold leading-8 text-white">
-                    {tier.name}
-                  </h2>
-                  {tier.mostPopular ? (
-                    <p className="rounded-full bg-indigo-500 px-2.5 py-1 text-xs font-semibold leading-5 text-white">
-                      Most popular
-                    </p>
-                  ) : null}
-                </div>
-                <p className="mt-4 text-sm leading-6 text-gray-300">{tier.description}</p>
-                <p className="mt-6 flex items-baseline gap-x-1">
-                  <span className="text-4xl font-bold tracking-tight text-white">
-                    {tier.price[frequency.value]}
-                  </span>
-                  <span className="text-sm font-semibold leading-6 text-gray-300">
-                    {frequency.priceSuffix}
-                  </span>
-                </p>
-                <a
-                  href={tier.href}
-                  aria-describedby={tier.id}
-                  className={
-                    classNames(
-                      tier.mostPopular
-                        ? 'bg-indigo-500 text-white shadow-sm hover:bg-indigo-400 focus-visible:outline-indigo-500'
-                        : 'bg-white/10 text-white hover:bg-white/20 focus-visible:outline-white',
-                      'mt-6 block rounded-md py-2 px-3 text-center text-sm font-semibold leading-6 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2'
-                    )
-                  }
-                >
-                  {tier.button}
-                </a>
-                <div className="text-center gap-2 mt-8 text-sm text-gray-400">
-                  <ClockIcon
-                    className="inline-block h-4 w-4 mr-1 text-blue-300 align-text-bottom"
-                    aria-hidden="true"
-                  />
-                  = Feature coming soon
-                </div>
-                <ul
-                  role="list"
-                  className="mt-8 space-y-3 text-sm leading-6 text-gray-300 xl:mt-10"
-                >
-                  {tier.name.includes('Basic') && (
-                    <p className="text-md font-bold uppercase my-4">Includes:</p>
-                  )}
-                  {tier.name.includes('Pro') && (
-                    <p className="text-md font-bold uppercase my-4">
-                      Everything in Basic, plus:
-                    </p>
-                  )}
-                  {tier.name.includes('Freelancer') && (
-                    <p className="text-md font-bold uppercase my-4">
-                      Everything in Pro, plus:
-                    </p>
-                  )}
-                  {tier.features.map((feature) => (
-                    <li key={feature.description} className="flex gap-x-3 items-center">
-                      {!feature.comingSoon ? (
-                        <CheckIcon
-                          className="h-6 w-5 flex-none text-white"
-                          aria-hidden="true"
-                        />
-                      ) : (
-                        <ClockIcon
-                          className="h-4 w-5 flex-none text-blue-300"
-                          aria-hidden="true"
-                        />
-                      )}
-                      {feature.description}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-          </div> */}
-        {/* </div> */}
-
-        {/* Testimonial section */}
-        {/* <div className="mx-auto mt-24 max-w-7xl px-6 sm:mt-56 lg:px-8">
-          <div className="mx-auto grid max-w-2xl grid-cols-1 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-            <div className="flex flex-col pb-10 sm:pb-16 lg:pb-0 lg:pr-8 xl:pr-20">
-              <img
-                className="h-12 self-start"
-                src="https://tailwindui.com/img/logos/tuple-logo-white.svg"
-                alt=""
-              />
-              <figure className="mt-10 flex flex-auto flex-col justify-between">
-                <blockquote className="text-lg leading-8 text-white">
-                  <p>
-                    “Amet amet eget scelerisque tellus sit neque faucibus non eleifend.
-                    Integer eu praesent at a. Ornare arcu gravida natoque erat et cursus
-                    tortor consequat at. Vulputate gravida sociis enim nullam ultricies
-                    habitant malesuada lorem ac. Tincidunt urna dui pellentesque
-                    sagittis.”
-                  </p>
-                </blockquote>
-                <figcaption className="mt-10 flex items-center gap-x-6">
-                  <img
-                    className="h-14 w-14 rounded-full bg-gray-800"
-                    src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <div className="text-base">
-                    <div className="font-semibold text-white">Judith Black</div>
-                    <div className="mt-1 text-gray-400">CEO of Tuple</div>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
-            <div className="flex flex-col border-t border-white/10 pt-10 sm:pt-16 lg:border-l lg:border-t-0 lg:pl-8 lg:pt-0 xl:pl-20">
-              <img
-                className="h-12 self-start"
-                src="https://tailwindui.com/img/logos/reform-logo-white.svg"
-                alt=""
-              />
-              <figure className="mt-10 flex flex-auto flex-col justify-between">
-                <blockquote className="text-lg leading-8 text-white">
-                  <p>
-                    “Excepteur veniam labore ullamco eiusmod. Pariatur consequat proident
-                    duis dolore nulla veniam reprehenderit nisi officia voluptate
-                    incididunt exercitation exercitation elit. Nostrud veniam sint dolor
-                    nisi ullamco.”
-                  </p>
-                </blockquote>
-                <figcaption className="mt-10 flex items-center gap-x-6">
-                  <img
-                    className="h-14 w-14 rounded-full bg-gray-800"
-                    src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
-                  />
-                  <div className="text-base">
-                    <div className="font-semibold text-white">Joseph Rodriguez</div>
-                    <div className="mt-1 text-gray-400">CEO of Reform</div>
-                  </div>
-                </figcaption>
-              </figure>
-            </div>
-          </div>
-        </div> */}
-
-        {/* FAQ section */}
-        {/* <div className="mx-auto mt-24 max-w-7xl px-6 sm:mt-56 lg:px-8">
-          <h2 className="text-2xl font-bold leading-10 tracking-tight text-white">
-            Frequently asked questions
-          </h2>
-          <p className="mt-6 max-w-2xl text-base leading-7 text-gray-300">
-            Have a different question and can’t find the answer you’re looking for? Reach
-            out to our support team by{' '}
-            <a href="#" className="font-semibold text-indigo-400 hover:text-indigo-300">
-              sending us an email
-            </a>{' '}
-            and we’ll get back to you as soon as we can.
-          </p>
-          <div className="mt-20">
-            <dl className="space-y-16 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:gap-y-16 sm:space-y-0 lg:grid-cols-3 lg:gap-x-10">
-              {faqs.map((faq) => (
-                <div key={faq.id}>
-                  <dt className="text-base font-semibold leading-7 text-white">
-                    {faq.question}
-                  </dt>
-                  <dd className="mt-2 text-base leading-7 text-gray-300">{faq.answer}</dd>
-                </div>
-              ))}
-            </dl>
-          </div>
-        </div> */}
       </main>
-
-      {/* Footer */}
-      {/* <footer className="mt-24 sm:mt-56" aria-labelledby="footer-heading">
-        <h2 id="footer-heading" className="sr-only">
-          Footer
-        </h2>
-        <div className="mx-auto max-w-7xl px-6 pb-8 lg:px-8">
-          <div className="border-t border-white/10 pt-16 sm:pt-24 xl:grid xl:grid-cols-3 xl:gap-8">
-            <div className="grid grid-cols-2 gap-8 xl:col-span-2">
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold leading-6 text-white">
-                    Solutions
-                  </h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.solutions.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className="text-sm leading-6 text-gray-300 hover:text-white"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6 text-white">Support</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.support.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className="text-sm leading-6 text-gray-300 hover:text-white"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-              <div className="md:grid md:grid-cols-2 md:gap-8">
-                <div>
-                  <h3 className="text-sm font-semibold leading-6 text-white">Company</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.company.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className="text-sm leading-6 text-gray-300 hover:text-white"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-                <div className="mt-10 md:mt-0">
-                  <h3 className="text-sm font-semibold leading-6 text-white">Legal</h3>
-                  <ul role="list" className="mt-6 space-y-4">
-                    {footerNavigation.legal.map((item) => (
-                      <li key={item.name}>
-                        <a
-                          href={item.href}
-                          className="text-sm leading-6 text-gray-300 hover:text-white"
-                        >
-                          {item.name}
-                        </a>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="mt-10 xl:mt-0">
-              <h3 className="text-sm font-semibold leading-6 text-white">
-                Subscribe to our newsletter
-              </h3>
-              <p className="mt-2 text-sm leading-6 text-gray-300">
-                The latest news, articles, and resources, sent to your inbox weekly.
-              </p>
-              <form className="mt-6 sm:flex sm:max-w-md">
-                <label htmlFor="email-address" className="sr-only">
-                  Email address
-                </label>
-                <input
-                  type="email"
-                  name="email-address"
-                  id="email-address"
-                  autoComplete="email"
-                  required
-                  className="w-full min-w-0 appearance-none rounded-md border-0 bg-white/5 px-3 py-1.5 text-base text-white shadow-sm ring-1 ring-inset ring-white/10 placeholder:text-gray-500 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:w-64 sm:text-sm sm:leading-6 xl:w-full"
-                  placeholder="Enter your email"
-                />
-                <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
-                  <button
-                    type="submit"
-                    className="flex w-full items-center justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
-                  >
-                    Subscribe
-                  </button>
-                </div>
-              </form>
-            </div>
-          </div>
-          <div className="mt-16 border-t border-white/10 pt-8 sm:mt-20 md:flex md:items-center md:justify-between lg:mt-24">
-            <div className="flex space-x-6 md:order-2">
-              {footerNavigation.social.map((item) => (
-                <a
-                  key={item.name}
-                  href={item.href}
-                  className="text-gray-500 hover:text-gray-400"
-                >
-                  <span className="sr-only">{item.name}</span>
-                  <item.icon className="h-6 w-6" aria-hidden="true" />
-                </a>
-              ))}
-            </div>
-            <p className="mt-8 text-xs leading-5 text-gray-400 md:order-1 md:mt-0">
-              &copy; 2020 Your Company, Inc. All rights reserved.
-            </p>
-          </div>
-        </div>
-      </footer> */}
     </div>
   );
 }
