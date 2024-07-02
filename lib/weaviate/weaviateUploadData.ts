21214cb95017bdd4c1a0a7b3ba199e2541c23a3e
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-02 19:58:54 +0200

    Added better readme fetch route behind user initated action + API call to upload - still testing

---
+++ lib/weaviate/weaviateUploadData.ts
@@ -0,0 +1,43 @@
+import client from './weaviateInitiateClient';
+
+export type WeaviateObject = {
+  name: string;
+  description: string;
+  tags: string[];
+  readme: string;
+};
+
+const weaviateUploadData = async (userProjectData: any, repoId?: string) => {
+  repoId = repoId || '519774186';
+  try {
+    const projectsCollection = client.collections.get('Projects');
+
+    const response = await projectsCollection.query.fetchObjectById(repoId);
+
+    // console.log(response?.properties)
+
+    if (response?.properties) {
+      console.log(
+        `Project ${response.properties.name} already exists in Weaviate - skipping upload`
+      );
+      return response;
+    }
+
+    await projectsCollection.data.insert({
+      name: userProjectData.name,
+      description: userProjectData.description,
+      tags: userProjectData.tags,
+      readme: userProjectData.readme,
+      // id: repoId,
+    });
+
+    console.log(`Inserted project ${userProjectData.name}`);
+
+    // return project;
+  } catch (error) {
+    console.error('Error uploading project to Weaviate:', error);
+    throw error;
+  }
+};
+
+export default weaviateUploadData;
