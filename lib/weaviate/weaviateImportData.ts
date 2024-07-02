4549061193f7da2a978d7e6f3b03e028c40b6c27
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-02 14:17:41 +0200

    Setting up initial test for github integration + Weaviate import

---
+++ lib/weaviate/weaviateImportData.ts
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import DOMPurify from 'dompurify';
+import client from './weaviateInitiateClient';
+import { useEffect, useState } from 'react';
+
+const importData = async () => {
+
+  // Hardcoding test data temporarily
+  const userId = 'bO4o8u9IskNbFk2wXZmjtJhAYkR2';
+  const userName = 'dannydoesdev';
+  const repoName = 'gitconnect-v2'
+  const repoId = '519774186';
+  const repoIdTwo = '572895196';
+  const repoNameTwo = 'starwars-search-api'
+
+  // Capture readme content in state
+  const [readme, setReadme] = useState('');
+
+  useEffect(() => {
+
+    // Get the readme content for public repo from the GitHub API
+    const handleImportReadme = async () => {
+
+      const readmeUrl = `/api/profiles/projects/edit/readme`;
+      axios
+        .get(readmeUrl, {
+          params: {
+            owner: userName,
+            repo: repoName,
+          },
+        })
+        .then((response) => {
+          const sanitizedHTML = DOMPurify.sanitize(response.data);
+          console.log(sanitizedHTML);
+          // setReadme(sanitizedHTML);
+  
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    };
+  
+    handleImportReadme();
+  }, []);
+  
+  // Object to be inserted into Weaviate - this will be replaced with the actual data
+  let dataObjects = [
+    {
+      name: 'GitConnect',
+      description: 'GitConnect is a platform for developers to showcase their projects and connect with other developers.',
+      tags: ['Full Stack', 'NextJs', 'React', 'Typescript'],
+      readme: readme,
+    },
+  ];
+
+  const weaviateProjectsCollection = client.collections.get('Projects');
+
+  const response = await weaviateProjectsCollection.data.insertMany(dataObjects);
+
+  console.log(response);
+
+};
+
+export default importData;
