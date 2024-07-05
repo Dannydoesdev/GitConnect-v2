209d73959ae8e199ced352378b8be5069ce71783
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-05 15:49:13 +0200

    Significant UI overhaul - reintegrated Mantine components to speed up completion of project, Fetch and upload pages done - query page to refactor and finish

---
+++ pages/api/weaviate/weaviatePortfolioSummaryRoute.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { generateUserWorkSummary } from '@/lib/weaviate/generateWeaviateResponse';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Extract username data from request params
+  const { username } = req.query;
+  
+  if (!username) {
+    return res.status(400).json({ error: 'username is required' });
+  }
+
+  try {
+    // Call the weaviate response generation helper 
+    console.log(`Portfolio summary generation handler called with username: ${username}`)
+
+  const generatedReponse = await generateUserWorkSummary(username.toString());
+    res.status(200).json(generatedReponse);
+  } catch (error) {
+    console.error('Error uploading to Weaviate:', error);
+    res.status(500).json({ error: `Error generating portfolio summary from Weaviate with error: ${error}` });
+  }
+}
