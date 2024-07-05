d6ffa297f0421dd396152d326a5c55f7ffbee2ff
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-05 12:15:27 +0200

    Updated schemas to refer to generative model, success utilising description generation + general query of weaviate repos - UI to finish

---
+++ gitconnect-v2-next/pages/api/weaviate/weaviateGenerateDescriptionRoute.ts
@@ -0,0 +1,22 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { generateProjectDescription } from '@/lib/weaviate/generateWeaviateResponse';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    // Extract project data from request params
+  const { username, reponame } = req.query;
+  
+  if (!username || !reponame) {
+    return res.status(400).json({ error: 'username and reponame are required' });
+  }
+
+  try {
+    // Call the weaviate response generation helper 
+    console.log(`Project summary generation handler called with username: ${username} and reponame: ${reponame}`)
+
+  const generatedReponse = await generateProjectDescription(username.toString(), reponame.toString());
+    res.status(200).json(generatedReponse);
+  } catch (error) {
+    console.error('Error uploading to Weaviate:', error);
+    res.status(500).json({ error: `Error uploading to Weaviate with error: ${error}` });
+  }
+}
