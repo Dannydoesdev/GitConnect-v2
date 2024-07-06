e1f2b1542e0946ded748042fae64d53e13a34d5b
Daniel McGee <daniel.t.mcgee@gmail.com>
2024-07-06 18:11:08 +0200

    Found hybrid search to work slightly better for project summaries - have left keyword search for the dynamic query

--- gitconnect-v2-next/lib/weaviate/generateWeaviateResponse.ts
+++ gitconnect-v2-next/lib/weaviate/generateWeaviateResponse.ts
@@ -19,6 +19,26 @@ export async function generateDynamicResponse(
   const inerpretedQuery = `Respond using HTML formatting - write a comprehensive answer to the following query on the user ${username}'s stored github repositories - ${query}`;
   console.log(`Query: ${inerpretedQuery}`);
 
+  const hybridResult = await myCollection.generate.hybrid(
+    `${username}, ${query}`,
+    {
+      groupedTask: inerpretedQuery,
+      groupedProperties: [
+        'name',
+        'description',
+        'readme',
+        'username',
+        // 'language_breakdown_percent',
+      ],
+    },
+    {
+      alpha: 0.5,
+      limit: 3,
+      filters: myCollection.filter.byProperty('username').equal(username),
+      // returnProperties: ['name', 'description', 'readme', 'tags'],
+    }
+  );
+
   const result = await myCollection.generate.nearText(
     `${username}, ${query}`,
     {
@@ -40,6 +60,7 @@ export async function generateDynamicResponse(
   console.log(`full response: ${JSON.stringify(result, null, 2)}`)
   console.log(result.generated);
   return result.generated;
+  // return hybridResult.generated;
 }
 
 export async function generateProjectDescription(username: string, reponame: string) {
@@ -57,6 +78,23 @@ export async function generateProjectDescription(username: string, reponame: str
 
 
   const interpretedQuery = `You are an AI assisting in writing a narrative for a software development project which they can use in their portfolio. Craft a story from the first-person perspective for the user ${username}'s github project named ${reponame}. Your answer should directly delve into the project's journey, avoiding formal introductions like 'Welcome to the narrative.' Focus on the project's purpose, challenges, technologies used, and outcomes, portraying it as a developer's personal recount. Provide the narrative in plain HTML format, using clear headings, paragraphs, and lists. Avoid any Markdown syntax like triple backticks; only use standard HTML tags. Keep the tone professional yet personal, reflecting the developer's voice and experience. Ensure the narrative is engaging, cohesive, professional, and informative, and is suitable for a developer's portfolio.`
+
+
+  const hybridResponse = await myCollection.generate.hybrid(
+    `${reponame}`,
+    {
+      singlePrompt: `Act as an expert in writing narratives for software development projects. Craft a story from the first-person perspective for the user ${username}'s github project named ${reponame} which they can use in their portfolio. Focus on the project's purpose, challenges, technologies used, and outcomes, portraying it as a developer's personal recount. Provide the narrative in plain HTML format, using clear headings, paragraphs, and lists. Avoid any Markdown syntax like triple backticks; only use standard HTML tags. Keep the tone professional yet personal, reflecting the developer's voice and experience. Ensure the narrative is engaging, cohesive, professional, and informative, and is suitable for a developer's portfolio. Utilise the following details to create this narrative: {name}, {description}, {readme}, {username}`,
+      // singlePrompt: `Respond using HTML formatting - write a comprehensive and compelling project description suitable for a software developer's portfolio page on the user ${username}'s github project named ${reponame} `,
+      // using the following details: {name}, {description}, {readme}, {username}`,
+    },
+    {
+      alpha: 0.5,
+      limit: 1,
+      filters: myCollection.filter.byProperty('name').equal(reponame),
+      // returnProperties: ['name', 'description', 'readme', 'tags', 'username'],
+    }
+
+  )
 
   const response = await myCollection.generate.nearText(
     `${reponame}`,
@@ -79,8 +117,8 @@ export async function generateProjectDescription(username: string, reponame: str
     console.log(obj.properties['name']);
     console.log(`Generated output: ${obj.generated}`);
   }
-
-  return response.objects[0].generated;
+  return hybridResponse.objects[0].generated;
+  // return response.objects[0].generated;
 }
 
 export async function generateUserWorkSummary(username: string) {
