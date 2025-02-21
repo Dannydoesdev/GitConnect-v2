caed45a552316982f058d0aa66aa4d0e2de7a6fb
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-02-21 18:48:20 +0100

    corrected info saved to repo subcollections

--- lib/quickstart/fetchReadme.ts
+++ lib/quickstart/fetchReadme.ts
@@ -1,17 +1,56 @@
 import axios from 'axios';
 import removeMarkdown from 'remove-markdown';
+import { Octokit } from '@octokit/rest';
+
 
 // Remove markdown syntax and newlines
-export const cleanMarkdown = (rawText: string) => {
+export const cleanMarkdown = (rawText: any) => {
   const strippedMarkdown = removeMarkdown(rawText);
   return strippedMarkdown.replace(/\n/g, ' ');
 };
+
+
+export const fetchReadmeNoapi = async (
+  userName: string,
+  repoName: string
+) => {
+
+  try {
+
+    const octokit = new Octokit();
+    const { data: readme } = await octokit.repos.getReadme({
+      owner: userName.toString(),
+      repo: repoName.toString(),
+      mediaType: {
+        format: 'raw', // fetch the raw markdown content
+      },
+    });
+
+    console.log('readme:', readme);
+
+    const readmeContent =  cleanMarkdown(readme);
+     
+
+    console.log('cleaned readmeContent:', readmeContent);
+
+    return readmeContent;
+  
+
+  } catch (error) {
+    console.error('Error fetching README from GitHub:', error);
+    return null;
+  }
+
+}
+
 
 // Utility function to fetch readme content for a given Github repository based on username and repo name
 export const fetchReadme = async (
   userName: string,
   repoName: string
 ): Promise<string | null> => {
+
+ 
   const readmeUrl = `/api/quickstart/fetchReadme`;
 
   try {
