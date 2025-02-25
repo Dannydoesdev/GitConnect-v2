623bb70567fc4cfc0b01bb52ade3524bc5e7a639
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-02-25 14:52:41 +0100

    Cleanup commented out code in quickstart files

--- components/Quickstart/ProfilePage/ProfilePageUserPanel/ProfilePageUserPanelSettingsModal.tsx
+++ components/Quickstart/ProfilePage/ProfilePageUserPanel/ProfilePageUserPanelSettingsModal.tsx
@@ -30,30 +30,6 @@ const URLInput = ({ label, form, name }: URLInputProps) => (
   <TextInput label={label} placeholder={`${label} URL`} {...form.getInputProps(name)} />
 );
 
-// const extractUsernameFromURL = (url: string, platform: string) => {
-//   const regex = new RegExp(`(https?://)?(www\\.)?${platform}\\.com/([a-zA-Z0-9_.-]+)`);
-//   const match = url.match(regex);
-//   return match ? match[3] : url;
-// };
-
-// TODO: Test the below implementations against different edge cases:
-
-// const extractUsernameFromURL = (url: string, platform: string) => {
-//   const regex = new RegExp(`(https?://)?(www\\.)?${platform}\\.\\w+\\.*\\w*\\.*\\w*\\/([a-zA-Z0-9_.-]+)`);
-//   const match = url.match(regex);
-//   return match ? match[3] : url;
-// };
-
-// const extractUsernameFromURL = (url: string, platform: string) => {
-//   // Check if the input is already a username
-//   if (!url.includes(platform)) return url;
-
-//   // Generalized regex to handle malformed URLs for all platforms
-//   const regex = new RegExp(`${platform}\\.\\w+\\.*\\w*\\.*\\w*\\/([a-zA-Z0-9_.-]+)`);
-//   const match = url.match(regex);
-//   return match ? match[1] : url;
-// };
-
 const extractUsernameFromURL = (url: string, platform: string) => {
   url = url.toLowerCase();
   platform = platform.toLowerCase();
@@ -164,68 +140,6 @@ const formatWebsiteURL = (input: string): string => {
   // If not, prepend http://
   return `https://${input}`;
 };
-
-// const extractUsernameFromURL = (url: string, platform: string) => {
-//   // Check if the input is already a username
-//   if (!url.includes(platform)) return url;
-
-//   // Generalized regex to handle malformed URLs for all platforms
-//   const regex = new RegExp(`${platform}[\\w.]*\\/in\\/([a-zA-Z0-9_.-]+)`);
-//   const match = url.match(regex);
-//   return match ? match[1] : url;
-// };
-
-// const extractUsernameFromURL = (url: string, platform: string) => {
-//   // Check if the input is already a username
-//   if (!url.includes(platform)) return url;
-
-//   // Generalized regex to handle malformed URLs for all platforms
-//   const regex = new RegExp(`${platform}[\\w.]*\\/([a-zA-Z0-9_.-]+)`);
-//   const match = url.match(regex);
-//   return match ? match[1] : url;
-// };
-
-// const formatSocialURL = (input: string, platform: string) => {
-//   if (!input) return '';
-
-//    // Extract username from the input URL or partial URL
-//    const username = extractUsernameFromURL(input, platform);
-
-//   switch (platform) {
-//     case 'github':
-//       return `https://github.com/${username}`;
-//     case 'linkedin':
-//       return `https://www.linkedin.com/in/${username}`;
-//     case 'twitter':
-//       return `https://twitter.com/${username}`;
-//     case 'medium':
-//       return `https://medium.com/@${username}`;
-//     case 'hashnode':
-//       return `https://${username}.hashnode.dev`;
-//     case 'codepen':
-//       return `https://codepen.io/${username}`;
-//     case 'dribbble':
-//       return `https://dribbble.com/${username}`;
-//     case 'behance':
-//       return `https://www.behance.net/${username}`;
-//     case 'devTo':
-//       return `https://dev.to/${username}`;
-//     case 'youtube':
-//       return `https://www.youtube.com/user/${username}`;
-//     case 'twitch':
-//       return `https://www.twitch.tv/${username}`;
-//     case 'discord':
-//       return `https://discord.com/users/${username}`;
-//     case 'stackoverflow':
-//       return `https://stackoverflow.com/users/${username}`;
-//     case 'facebook':
-//       return `https://www.facebook.com/${username}`;
-//     case 'instagram':
-//       return `https://www.instagram.com/${username}`;
-//     default:
-//       return '';
-//   }
-// };
 
 interface ProfilePageUserPanelSettingsProps {
   opened: boolean;
@@ -382,139 +296,6 @@ const ProfilePageUserPanelSettings = ({
     });
   }, [formData, props]);
 
-  // console.log("formData:", formData);
-  // console.log("props:", props);
-  // console.log("dataToShow:", dataToShow);
-  // console.log("form.values:", form.values);
-
-  // Attempt #1 to resolve discrepencies and blank form fields
-
-  // const form = useForm({
-  //   initialValues: {
-  //     name: dataToShow.name || '',
-  //     location: dataToShow.location || '',
-  //     email: dataToShow.email || '',
-  //     publicEmail: dataToShow.publicEmail || '',
-  //     website: dataToShow.website || '',
-  //     headline: dataToShow.headline || '',
-  //     company: dataToShow.company || '',
-  //     position: dataToShow.position || '',
-  //     techStack: dataToShow.techStack || [],
-  //     skills: dataToShow.skills || [],
-  //     openToWork: dataToShow.openToWork || false,
-  //     // visibleToPublic: dataToShow.visibleToPublic || false,
-  //     profileTags: dataToShow.profileTags || [],
-  //     bio: dataToShow.bio || '',
-  //     githubUrl: dataToShow.githubUrl || '',
-  //     gitlabUrl: dataToShow.gitlabUrl || '',
-  //     linkedinUrl: dataToShow.linkedinUrl || '',
-  //     twitterUrl: dataToShow.twitterUrl || '',
-  //     mediumUrl: dataToShow.mediumUrl || '',
-  //     hashnodeUrl: dataToShow.hashnodeUrl || '',
-  //     codepenUrl: dataToShow.codepenUrl || '',
-  //     dribbbleUrl: dataToShow.dribbbleUrl || '',
-  //     behanceUrl: dataToShow.behanceUrl || '',
-  //     devToUrl: dataToShow.devToUrl || '',
-  //     youtubeUrl: dataToShow.youtubeUrl || '',
-  //     twitchUrl: dataToShow.twitchUrl || '',
-  //     discordUrl: dataToShow.discordUrl || '',
-  //     stackoverflowUrl: dataToShow.stackoverflowUrl || '',
-  //     facebookUrl: dataToShow.facebookUrl || '',
-  //     instagramUrl: dataToShow.instagramUrl || '',
-  //     githubUsername: dataToShow.githubUsername || '',
-  //     gitlabUsername: dataToShow.gitlabUsername || '',
-  //     linkedinUsername: dataToShow.linkedinUsername || '',
-  //     twitterUsername: dataToShow.twitterUsername || '',
-  //     mediumUsername: dataToShow.mediumUsername || '',
-  //     hashnodeUsername: dataToShow.hashnodeUsername || '',
-  //     codepenUsername: dataToShow.codepenUsername || '',
-  //     dribbbleUsername: dataToShow.dribbbleUsername || '',
-  //     behanceUsername: dataToShow.behanceUsername || '',
-  //     devToUsername: dataToShow.devToUsername || '',
-  //     youtubeUsername: dataToShow.youtubeUsername || '',
-  //     twitchUsername: dataToShow.twitchUsername || '',
-  //     discordUsername: dataToShow.discordUsername || '',
-  //     stackoverflowUsername: dataToShow.stackoverflowUsername || '',
-  //     facebookUsername: dataToShow.facebookUsername || '',
-  //     instagramUsername: dataToShow.instagramUsername || '',
-  //   },
-  // });
-
-  // OLD - DOESNT WORK
-  // const form = useForm({
-  //   initialValues: {
-  //     name: props.name || '',
-  //     location: props.location || '',
-  //     email: props.email || '',
-  //     publicEmail: props.publicEmail || '',
-  //     website: props.website || '',
-  //     headline: props.headline || '',
-  //     company: props.company || '',
-  //     position: props.position || '',
-  //     techStack: props.techStack || [],
-  //     skills: props.skills || [],
-  //     openToWork: props.openToWork || false,
-  //     // visibleToPublic: props.visibleToPublic || false,
-  //     profileTags: props.profileTags || [],
-  //     bio: props.bio || '',
-
-  //     githubUrl: props.githubUrl || '',
-  //     gitlabUrl: props.gitlabUrl || '',
-  //     linkedinUrl: props.linkedinUrl || '',
-  //     twitterUrl: props.twitterUrl || '',
-  //     mediumUrl: props.mediumUrl || '',
-  //     hashnodeUrl: props.hashnodeUrl || '',
-  //     codepenUrl: props.codepenUrl || '',
-  //     dribbbleUrl: props.dribbbleUrl || '',
-  //     behanceUrl: props.behanceUrl || '',
-  //     devToUrl: props.devToUrl || '',
-  //     youtubeUrl: props.youtubeUrl || '',
-  //     twitchUrl: props.twitchUrl || '',
-  //     discordUrl: props.discordUrl || '',
-  //     stackoverflowUrl: props.stackoverflowUrl || '',
-  //     facebookUrl: props.facebookUrl || '',
-  //     instagramUrl: props.instagramUrl || '',
-
-  //     githubUsername: props.githubUsername || '',
-  //     gitlabUsername: props.gitlabUsername || '',
-  //     linkedinUsername: props.linkedinUsername || '',
-  //     twitterUsername: props.twitterUsername || '',
-  //     mediumUsername: props.mediumUsername || '',
-  //     hashnodeUsername: props.hashnodeUsername || '',
-  //     codepenUsername: props.codepenUsername || '',
-  //     dribbbleUsername: props.dribbbleUsername || '',
-  //     behanceUsername: props.behanceUsername || '',
-  //     devToUsername: props.devToUsername || '',
-  //     youtubeUsername: props.youtubeUsername || '',
-  //     twitchUsername: props.twitchUsername || '',
-  //     discordUsername: props.discordUsername || '',
-  //     stackoverflowUsername: props.stackoverflowUsername || '',
-  //     facebookUsername: props.facebookUsername || '',
-  //     instagramUsername: props.instagramUsername || '',
-  //   },
-  // });
-
-  // Data for MultiSelect
-  // const [data, setData] = useState([
-  //   { value: 'react', label: 'React', group: 'Frontend' },
-  //   { value: 'javascript', label: 'Javascript', group: 'Frontend' },
-  //   { value: 'typescript', label: 'TypeScript', group: 'Frontend' },
-  //   { value: 'nextjs', label: 'Next.js', group: 'Backend' },
-  //   { value: 'nodejs', label: 'Node.js', group: 'Backend' },
-  //   { value: 'firebase', label: 'Firebase', group: 'Backend' },
-  //   { value: 'python', label: 'Python', group: 'Backend' },
-  //   { value: 'flask', label: 'Flask', group: 'Frontend' },
-  //   { value: 'sql', label: 'SQL', group: 'Database' },
-  //   { value: 'firestore', label: 'Firestore', group: 'Database' },
-  //   { value: 'mongodb', label: 'MongoDB', group: 'Database' },
-  //   { value: 'html', label: 'HTML', group: 'Frontend' },
-  //   { value: 'css', label: 'CSS', group: 'Frontend' },
-  //   { value: 'tailwindcss', label: 'Tailwind CSS', group: 'Styling + Components' },
-  //   { value: 'bootstrap', label: 'Bootstrap', group: 'Styling + Components' },
-  //   { value: 'materialui', label: 'Material UI', group: 'Styling + Components' },
-  //   { value: 'chakraui', label: 'Chakra UI', group: 'Styling + Components' },
-  // ]);
-
   // Updated options:
 
   const [data, setData] = useState([
@@ -568,7 +349,6 @@ const ProfilePageUserPanelSettings = ({
     { value: 'chakraui', label: 'Chakra UI', group: 'Styling + Components' },
     { value: 'antdesign', label: 'Ant Design', group: 'Styling + Components' },
     { value: 'materializecss', label: 'Materialize CSS', group: 'Styling + Components' },
-
   ]);
 
   const handleSubmit = () => {
@@ -628,41 +408,6 @@ const ProfilePageUserPanelSettings = ({
     handleSaveChanges(formattedValues);
   };
 
-  // const handleSubmit = () => {
-  //   const formattedValues = {
-  //     ...form.values,
-  //     githubUrl: formatSocialURL(form.values.githubUrl, 'github'),
-  //     linkedinUrl: formatSocialURL(form.values.linkedinUrl, 'linkedin'),
-  //     twitterUrl: formatSocialURL(form.values.twitterUrl, 'twitter'),
-  //     mediumUrl: formatSocialURL(form.values.mediumUrl, 'medium'),
-  //     hashnodeUrl: formatSocialURL(form.values.hashnodeUrl, 'hashnode'),
-  //     codepenUrl: formatSocialURL(form.values.codepenUrl, 'codepen'),
-  //     dribbbleUrl: formatSocialURL(form.values.dribbbleUrl, 'dribbble'),
-  //     behanceUrl: formatSocialURL(form.values.behanceUrl, 'behance'),
-  //     devToUrl: formatSocialURL(form.values.devToUrl, 'devTo'),
-  //     youtubeUrl: formatSocialURL(form.values.youtubeUrl, 'youtube'),
-  //     twitchUrl: formatSocialURL(form.values.twitchUrl, 'twitch'),
-  //     discordUrl: formatSocialURL(form.values.discordUrl, 'discord'),
-  //     stackoverflowUrl: formatSocialURL(form.values.stackoverflowUrl, 'stackoverflow'),
-  //     facebookUrl: formatSocialURL(form.values.facebookUrl, 'facebook'),
-  //     instagramUrl: formatSocialURL(form.values.instagramUrl, 'instagram'),
-
-  // mediumUrl: formatSocialURL(form.values.mediumUsername, 'medium'),
-  // hashnodeUrl: formatSocialURL(form.values.hashnodeUsername, 'hashnode'),
-  // codepenUrl: formatSocialURL(form.values.codepenUsername, 'codepen'),
-  // dribbbleUrl: formatSocialURL(form.values.dribbbleUsername, 'dribbble'),
-  // behanceUrl: formatSocialURL(form.values.behanceUsername, 'behance'),
-  // devToUrl: formatSocialURL(form.values.devToUsername, 'devTo'),
-  // youtubeUrl: formatSocialURL(form.values.youtubeUsername, 'youtube'),
-  // twitchUrl: formatSocialURL(form.values.twitchUsername, 'twitch'),
-  // discordUrl: formatSocialURL(form.values.discordUsername, 'discord'),
-  // stackoverflowUrl: formatSocialURL(form.values.stackoverflowUsername, 'stackoverflow'),
-  // facebookUrl: formatSocialURL(form.values.facebookUsername, 'facebook'),
-  // instagramUrl: formatSocialURL(form.values.instagramUsername, 'instagram'),
-  //   };
-  //   handleSaveChanges(formattedValues);
-  // };
-
   return (
     <Modal
       size="xl"
@@ -689,10 +434,7 @@ const ProfilePageUserPanelSettings = ({
           placeholder="The email you want users to see - feel free to leave this blank"
           {...form.getInputProps('publicEmail')}
         />
-        {/* <TextInput label="Name" {...form.getInputProps('name')} /> */}
-        {/* <TextInput label="Location" {...form.getInputProps('location')} /> */}
-        {/* <TextInput label="Email" {...form.getInputProps('email')} /> */}
-        {/* <TextInput label="Public Contact Email" {...form.getInputProps('publicEmail')} /> */}
+
         <TextInput
           label="Personal Website"
           placeholder="Your personal website, portfolio etc"
@@ -713,10 +455,7 @@ const ProfilePageUserPanelSettings = ({
           placeholder="Early Adopter"
           {...form.getInputProps('position')}
         />
-        {/* <Switch */}
-        {/* <TextInput label="Headline" {...form.getInputProps('headline')} />
-        <TextInput label="Company" {...form.getInputProps('company')} />
-        <TextInput label="Position" {...form.getInputProps('position')} /> */}
+
         <Switch
           label="Open To Work"
           {...form.getInputProps('openToWork', { type: 'checkbox' })}
@@ -727,8 +466,6 @@ const ProfilePageUserPanelSettings = ({
         <Textarea
           label="Bio"
           placeholder="A passionate developer who loves playing with new technology & building impactful web apps. I keep up to date with the developer community through awesome platforms like GitConnect..."
-          // placeholder="Passionate developer,  learning and excited about building impactful web apps..."
-          // placeholder="Passionate developer with 5 years of experience in building web apps..."
           autosize
           minRows={2}
           maxRows={7}
@@ -881,70 +618,9 @@ const ProfilePageUserPanelSettings = ({
               placeholder="e.g. johndoesgram"
               {...form.getInputProps('instagramUsername')}
             />
-            {/* <TextInput label="Github Username" {...form.getInputProps('githubUsername')} />
-          <TextInput
-            label="Linkedin Username"
-            {...form.getInputProps('linkedinUsername')}
-          />
-          <TextInput
-            label="Twitter Username"
-            {...form.getInputProps('twitterUsername')}
-          />
-          <TextInput label="Medium Username" {...form.getInputProps('mediumUsername')} />
-          <TextInput
-            label="Hashnode Username"
-            {...form.getInputProps('hashnodeUsername')}
-          />
-          <TextInput
-            label="Codepen Username"
-            {...form.getInputProps('codepenUsername')}
-          />
-          <TextInput
-            label="Dribbble Username"
-            {...form.getInputProps('dribbbleUsername')}
-          />
-          <TextInput
-            label="Behance Username"
-            {...form.getInputProps('behanceUsername')}
-          />
-          <TextInput label="Dev.to Username" {...form.getInputProps('devToUsername')} />
-          <TextInput
-            label="Youtube Username"
-            {...form.getInputProps('youtubeUsername')}
-          />
-          <TextInput label="Twitch Username" {...form.getInputProps('twitchUsername')} />
-
-          <TextInput
-            label="Stackoverflow Username"
-            {...form.getInputProps('stackoverflowUsername')}
-          /> */}
-            {/* <TextInput
-            label="Facebook Username"
-            {...form.getInputProps('facebookUsername')}
-          /> */}
-            {/* <TextInput
-            label="Discord Username"
-            {...form.getInputProps('discordUsername')}
-          /> */}
-            {/* <TextInput
-            label="Instagram Username"
-            {...form.getInputProps('instagramUsername')}
-          /> */}
           </Stack>
         </Spoiler>
-        {/* <Affix
-          bg={'black'}
-          // position={{ bottom: 0 }}
-          // bottom='30%'
-          // ml={40}
-          right='30%'
-          bottom={0}
-          left={0}
-          // left='50%'
-          // right='70%'
-          // top='70%'
-          zIndex={300}
-       > */}
+
         <Group position="center" pb="lg">
           {/* <Button variant="filled" fullWidth mt="md" onClick={() => handleSaveChanges(form.values)}> */}
           <Button variant="filled" radius="sm" w="40%" mt="md" onClick={handleSubmit}>
@@ -968,23 +644,3 @@ const ProfilePageUserPanelSettings = ({
 };
 
 export default ProfilePageUserPanelSettings;
-
-// {
-/* <URLInput label="Github" form={form} name="githubUrl" />
-        <URLInput label="Gitlab" form={form} name="gitlabUrl" />
-        <URLInput label="LinkedIn" form={form} name="linkedinUrl" />
-        <URLInput label="Twitter" form={form} name="twitterUrl" />
-        <URLInput label="Medium" form={form} name="mediumUrl" />
-        <URLInput label="Hashnode" form={form} name="hashnodeUrl" />
-        <URLInput label="Codepen" form={form} name="codepenUrl" />
-        <URLInput label="Dev.to" form={form} name="devToUrl" />
-        <URLInput label="Stack Overflow" form={form} name="stackoverflowUrl" />
-
-        <URLInput label="Dribbble" form={form} name="dribbbleUrl" />
-        <URLInput label="Behance" form={form} name="behanceUrl" />
-        <URLInput label="Youtube" form={form} name="youtubeUrl" />
-        <URLInput label="Twitch" form={form} name="twitchUrl" />
-        <URLInput label="Discord" form={form} name="discordUrl" />
-        <URLInput label="Instagram" form={form} name="instagramUrl" />
-        <URLInput label="Facebook" form={form} name="facebookUrl" /> */
-// }
--- lib/quickstart/saveQuickstart.ts
+++ lib/quickstart/saveQuickstart.ts
@@ -37,10 +37,6 @@ export async function saveQuickstartProject(
     const readme = await fetchReadmeNoapi(userName, repoName);
     const languages = await fetchLanguages(projectData.languages_url);
 
-    // console.log('readme in saveQuickstartProject:');
-    // console.log(readme);
-    // console.log('languages in saveQuickstartProject:');
-    // console.log(languages);
     // CHECK IF THIS PARENTDOC THING IS NEEDED:
 
     const parentDocRef = doc(db, `usersAnonymous/${userid}/repos/${repoid}`);
@@ -79,12 +75,6 @@ export async function saveQuickstartProject(
     }
   }
 
-  // const hiddenStatusRef = doc(db, `users/${userid}/repos/${repoid}`);
-
-  // await setDoc(hiddenStatusRef, { hidden: false }, { merge: true });
-  // console.log(formData)
-  // console.log('publishing');
-  // close();
 }
 
 // components/Portfolio/Project/EditProject/EditProject.tsx
@@ -113,58 +103,3 @@ export async function saveQuickstartProfile(userid: string, userData: any) {
   // await setDoc(publicDataDocRef, { ...userData }, { merge: true });
 }
 
-//  Save profiledata:
-
-// export async function updateProfileDataGithub(id: string, data: any) {
-//   // NOTE - this is the function for updating profile changes to firestore
-//   // I am duplicating this logic to publicData and githubData while we deprecate githubData
-//   // TODO - remove this logic once githubData is deprecated
-
-//   const publicDataDocRef = doc(db, `users/${id}/profileData/publicData`);
-
-//   // const docSnap = await getDoc(publicDataDocRef);
-
-//   const publicDataDocSnap = await getDoc(publicDataDocRef);
-//   await setDoc(publicDataDocRef, { ...data }, { merge: true });
-
-//   // console.log(`successfully added updated profile data to BOTH githubdata and publicData:`)
-//   // console.log(data)
-// }
-
-// // Sign up flow:
-// const signupHandler = useCallback(
-//   async (e: any) => {
-//     e.preventDefault();
-//     const provider = new GithubAuthProvider();
-
-//     try {
-//       // Attempt popup OAuth
-//       await signInWithPopup(auth, provider).then((result) => {
-//         const credential: any = GithubAuthProvider.credentialFromResult(result);
-//         const user = result.user;
-//         const userId = user.uid;
-
-//         if (process.env.NODE_ENV === 'development') {
-//           // mixpanel.init('13152890549909d8a9fe73e4daf06e43', { debug: true });
-//           // mixpanel.identify(userId);
-
-//           // mixpanel.track('Signed In', {
-//           //   'Signup Type': 'GitHub',
-//           // });
-//         } else {
-//           mixpanel.init('13152890549909d8a9fe73e4daf06e43', { debug: false });
-//           mixpanel.identify(userId);
-
-//           mixpanel.track('Signed up', {
-//             'Signup Type': 'GitHub',
-//           });
-//         }
-//       });
-
-//     } catch (error) {
-//       console.log(error);
-//       // alert(error)
-//     } finally {
-//       Router.push('/addproject');
-//     }
-//   },
