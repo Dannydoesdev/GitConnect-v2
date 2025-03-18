f1798995d6ba9a988920f9d0d9c751eaaa415b63
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-03-18 11:53:12 +0100

    added more portfolio project image placeholders to pick from

--- gitconnect-v2-next/pages/quickstart.tsx
+++ gitconnect-v2-next/pages/quickstart.tsx
@@ -291,6 +291,7 @@ const createPortfolioWithUsernameOnly = () => {
           );
         }
 
+          // FIXME: NESTED NEEDS CLEANUP
         signInAnonymously(auth)
           .then(async (userCredential) => {
             const user = userCredential.user;
