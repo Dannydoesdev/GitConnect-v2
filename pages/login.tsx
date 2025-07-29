f0e39b2ef32f01bf923f088ce371dd6ea9c90cf0
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-07-29 17:53:46 +0200

    cleanup login and signup pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,47 +1,22 @@
-import React, { useContext } from "react"
-import { AuthContext } from "../context/AuthContext"
-import { useRouter } from "next/router"
+import React, { useContext } from 'react';
+import { AuthContext } from '../context/AuthContext';
+import { useRouter } from 'next/router';
 // import GithubLogin from "../components//LoginPages/GithubLogin"
 // import EmailLogin from "../components/LoginPages/EmailLogin"
 // import GoogleLogin from "../components//LoginPages/GoogleLogin"
 // import { LoginPage } from "../components/LoginPages/Login"
-import { LoginPage } from "@/components/SignupPages/NewLogin"
-// import Mixpanel from "mixpanel"
+import { LoginPage } from '@/components/SignupPages/NewLogin';
 
 const Login = () => {
-  // const mixpanel = Mixpanel.init('13152890549909d8a9fe73e4daf06e43');
-  const { currentUser } = useContext(AuthContext)
-  const Router = useRouter()
-
-  // console.log('Auth State - login page:', {
-  //   currentUser: currentUser,
-  //   currentPath: Router.pathname
-  // });
+  const { currentUser } = useContext(AuthContext);
+  const Router = useRouter();
 
   if (currentUser) {
-    Router.push("/")
-    return <></>
+    Router.push('/');
+    return <></>;
   } else {
-    return (
-      
-      <LoginPage />
-      // <Login 
-      // <GithubLogin />
-
-      // <div className="grid place-content-center px-10 py-10 shadow-lg w-max mx-auto mt-6">
-      //   <h1 className="text-center font-black text-3xl mb-2">Sign in with Github</h1>
-      //   <div className="flex flex-col gap-y-3">
-      //   </div>
-      //   {/* <h1 className="text-center font-black text-3xl mb-2">Sign in with Email</h1>
-      //   <div className="flex flex-col gap-y-3">
-      //     <>
-      //       <GoogleLogin /> */}
-      //       {/* <EmailLogin /> */}
-      //     {/* </>
-      //   </div> */}
-      // </div>
-    )
+    return <LoginPage />;
   }
-}
+};
 
-export default Login
+export default Login;
