cf247a4e64f7dc4a7086d2e84d067ab7e5b008c8
Daniel McGee <daniel.t.mcgee@gmail.com>
2025-07-29 17:39:51 +0200

    cleanup api routes

--- gitconnect-v2-next/mantine/cache.ts
+++ gitconnect-v2-next/mantine/cache.ts
@@ -1,37 +1,8 @@
-import { MantineProvider, createEmotionCache } from '@mantine/core';
+import { createEmotionCache } from '@mantine/core';
 
-// append mantine to the end of the style import
+// Append mantine to the end of the style import
 
-export const mantineCache = createEmotionCache({ key: 'mantine', prepend: false });
-
-// usage:
-
-// function Demo() {
-//   return (
-//     <MantineProvider emotionCache={appendCache} withGlobalStyles withNormalizeCSS>
-//       <App />
-//     </MantineProvider>
-//   );
-// }
-
-
-// Create new cache
-
-// import { MantineProvider, createEmotionCache } from '@mantine/core';
-
-// const myCache = createEmotionCache({ key: 'mantine' });
-
-// function Demo() {
-//   return (
-//     <MantineProvider emotionCache={myCache} withGlobalStyles withNormalizeCSS>
-//       <App />
-//     </MantineProvider>
-//   );
-// }
-
-// To change classes prefix create cache with key:
-
-
-// import { MantineProvider, createEmotionCache } from '@mantine/core';
-
-// const myCache = createEmotionCache({ key: 'my-prefix' });
+export const mantineCache = createEmotionCache({
+  key: 'mantine',
+  prepend: false,
+});
--- gitconnect-v2-next/pages/api/portfolio/getTextEditorContent.ts
+++ gitconnect-v2-next/pages/api/portfolio/getTextEditorContent.ts
@@ -1,11 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { getProjectTextEditorContent } from '@/lib/projects';
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { userId, repoId } = req.query;
-  // console.log('getTextEditorContent API route hit with userId:', userId, 'and repoId:', repoId)
   try {
-    const data = await getProjectTextEditorContent(userId as string, repoId as string);
+    const data = await getProjectTextEditorContent(
+      userId as string,
+      repoId as string
+    );
     res.status(200).json({ data });
   } catch (error) {
     res.status(500).json({ error: (error as Error).message });
